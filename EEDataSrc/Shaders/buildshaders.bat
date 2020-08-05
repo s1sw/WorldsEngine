902047ae664edd21459df9bbdcd1c14b0277f36a
Someone Somewhere <sackboydj167@gmail.com>
2020-08-05 21:52:47 +0100

    Combined standard frag + vert shader into one file

--- EEDataSrc/Shaders/buildshaders.bat
+++ EEDataSrc/Shaders/buildshaders.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /D "%~dp0"
-glslangValidator standard.frag.glsl -V -o standard.frag.spv
-glslangValidator standard.vert.glsl -V -o standard.vert.spv
-glslangValidator tonemap.comp.glsl -V -o tonemap.comp.spv
-glslangValidator tonemap2d.comp.glsl -V -o tonemap2d.comp.spv
-glslangValidator clear_pick_buf.comp.glsl -V -o clear_pick_buf.comp.spv
-glslangValidator shadowmap.vert.glsl -V -o shadowmap.vert.spv
-glslangValidator shadowmap.frag.glsl -V -o shadowmap.frag.spv
-glslangValidator wire_obj.vert.glsl -V -o wire_obj.vert.spv
-glslangValidator wire_obj.frag.glsl -V -o wire_obj.frag.spv
+glslangValidator standard.glsl -DFRAGMENT -V -S frag -o standard.frag.spv 
+glslangValidator standard.glsl -DVERTEX -V -S vert -o standard.vert.spv 
+REM glslangValidator tonemap.comp.glsl -V -o tonemap.comp.spv
+REM glslangValidator tonemap2d.comp.glsl -V -o tonemap2d.comp.spv
+REM glslangValidator clear_pick_buf.comp.glsl -V -o clear_pick_buf.comp.spv
+REM glslangValidator shadowmap.vert.glsl -V -o shadowmap.vert.spv
+REM glslangValidator shadowmap.frag.glsl -V -o shadowmap.frag.spv
+REM glslangValidator wire_obj.vert.glsl -V -o wire_obj.vert.spv
+REM glslangValidator wire_obj.frag.glsl -V -o wire_obj.frag.spv
 move *.spv ../../EEData/Shaders
--- EEDataSrc/Shaders/buildshaders_debug.bat
+++ EEDataSrc/Shaders/buildshaders_debug.bat
@@ -1,10 +1,12 @@
 @echo off
-glslangValidator -g test.frag.glsl -V -o test.frag.spv
-glslangValidator -g test.vert.glsl -V -o test.vert.spv
+cd /D "%~dp0"
+glslangValidator -g standard.frag.glsl -V -o standard.frag.spv
+glslangValidator -g standard.vert.glsl -V -o standard.vert.spv
 glslangValidator -g tonemap.comp.glsl -V -o tonemap.comp.spv
+glslangValidator -g tonemap2d.comp.glsl -V -o tonemap2d.comp.spv
+glslangValidator -g clear_pick_buf.comp.glsl -V -o clear_pick_buf.comp.spv
 glslangValidator -g shadowmap.vert.glsl -V -o shadowmap.vert.spv
 glslangValidator -g shadowmap.frag.glsl -V -o shadowmap.frag.spv
 glslangValidator -g wire_obj.vert.glsl -V -o wire_obj.vert.spv
 glslangValidator -g wire_obj.frag.glsl -V -o wire_obj.frag.spv
 move *.spv ../../EEData/Shaders
-pause
