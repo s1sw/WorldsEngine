7cf104521e58946f755bfdfd53cb4bb7f11251d9
Someone Somewhere <someone167@pm.me>
2020-11-26 18:10:55 +0000

    log error on fatal crash

--- ExperimentalEngine/Fatal.cpp
+++ ExperimentalEngine/Fatal.cpp
@@ -1,7 +1,9 @@
 #include "Fatal.hpp"
+#include "Log.hpp"
 #include <SDL2/SDL_messagebox.h>
 
 void fatalErr(const char* msg) {
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Worlds Engine", msg, nullptr);
+    logErr(SDL_LOG_PRIORITY_CRITICAL, "%s", msg);
     abort();
 }
