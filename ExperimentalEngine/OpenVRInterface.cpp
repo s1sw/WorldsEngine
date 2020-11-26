c26c6001e2638db8ad080a34711ac7a9a49d6d1e
Someone Somewhere <someone167@pm.me>
2020-11-26 18:09:55 +0000

    split openvrinterface into different files
    Former-commit-id: dc7beccc90f5fcddc2bb8f5d2acaa17af4060989

---
+++ ExperimentalEngine/OpenVRInterface.cpp
@@ -0,0 +1,174 @@
+#include "OpenVRInterface.hpp"
+
+namespace worlds {
+    void OpenVRInterface::checkErr(vr::EVRInputError err) {
+        if (err != vr::VRInputError_None) {
+            logErr("VR Input Error: %i", err);
+        }
+    }
+
+    void OpenVRInterface::init() {
+        vr::EVRInitError eError = vr::VRInitError_None;
+        system = vr::VR_Init(&eError, vr::VRApplication_Scene);
+
+        if (eError != vr::VRInitError_None) {
+            std::string errMsg = std::string{"Unable to init VR runtime: "} + vr::VR_GetVRInitErrorAsEnglishDescription(eError);
+            fatalErr(errMsg.c_str());
+        }
+        
+        const char* basePath = SDL_GetBasePath();
+        
+
+        std::string actionsPathStr = basePath + std::string("EEData") + PATH_SEP + "actions.json";
+        const char* actionsPath = actionsPathStr.c_str();
+        logMsg("Using %s as actionsPath", actionsPath);
+        auto vrInput = vr::VRInput();
+        vrInput->SetActionManifestPath(actionsPath);
+        checkErr(vrInput->GetActionHandle("/actions/main/in/Movement", &movementAction));
+        checkErr(vrInput->GetActionHandle("/actions/main/in/LeftHand", &leftHand));
+        checkErr(vrInput->GetActionHandle("/actions/main/in/RightHand", &rightHand));
+        checkErr(vrInput->GetActionHandle("/actions/main/in/Sprint", &sprintAction));
+        checkErr(vrInput->GetActionHandle("/actions/main/in/Jump", &jumpAction));
+
+        checkErr(vrInput->GetActionSetHandle("/actions/main", &actionSet));
+    }
+
+    std::vector<std::string> OpenVRInterface::getVulkanInstanceExtensions() {
+        uint32_t extCharCount = vr::VRCompositor()->GetVulkanInstanceExtensionsRequired(nullptr, 0);
+
+        std::string str((size_t)extCharCount, 'Z');
+
+        vr::VRCompositor()->GetVulkanInstanceExtensionsRequired(str.data(), (uint32_t)str.size());
+
+        std::vector<std::string> extensions;
+
+        std::istringstream stringStream(str.substr(0, extCharCount));
+        std::string currExt;
+        while (std::getline(stringStream, currExt, ' ')) {
+            extensions.push_back(currExt);
+        }
+
+        return extensions;
+    }
+
+    std::vector<std::string> OpenVRInterface::getVulkanDeviceExtensions(VkPhysicalDevice physDevice) {
+        uint32_t extCharCount = vr::VRCompositor()->GetVulkanDeviceExtensionsRequired(physDevice, nullptr, 0);
+
+        std::string str((size_t)extCharCount, 'Z');
+
+        vr::VRCompositor()->GetVulkanDeviceExtensionsRequired(physDevice, str.data(), (uint32_t)str.size());
+
+        std::vector<std::string> extensions;
+
+        std::istringstream stringStream(str);
+        std::string currExt;
+        while (std::getline(stringStream, currExt, ' ')) {
+            extensions.push_back(currExt);
+        }
+
+        return extensions;
+    }
+
+    void composeProjection(float fLeft, float fRight, float fTop, float fBottom, float zNear, glm::mat4& p) {
+        float idx = 1.0f / (fRight - fLeft);
+        float idy = 1.0f / (fBottom - fTop);
+        float sx = fRight + fLeft;
+        float sy = fBottom + fTop;
+
+        p[0][0] = 2 * idx; p[1][0] = 0;       p[2][0] = sx * idx;    p[3][0] = 0;
+        p[0][1] = 0;       p[1][1] = 2 * idy; p[2][1] = sy * idy;    p[3][1] = 0;
+        p[0][2] = 0;       p[1][2] = 0;       p[2][2] = 0.0f; p[3][2] = zNear;
+        p[0][3] = 0;       p[1][3] = 0;       p[2][3] = -1.0f;       p[3][3] = 0;
+    }
+
+    void OpenVRInterface::getRenderResolution(uint32_t* x, uint32_t* y) {
+        system->GetRecommendedRenderTargetSize(x, y);
+    }
+
+    glm::mat4 OpenVRInterface::getProjMat(vr::EVREye eye, float near, float) {
+        float left, right, top, bottom;
+        system->GetProjectionRaw(eye, &left, &right, &top, &bottom);
+
+        glm::mat4 m;
+
+        composeProjection(left, right, top, bottom, near, m);
+        return m;
+    }
+
+    void OpenVRInterface::updateInput() {
+        vr::VRActiveActionSet_t activeActionSet{
+            .ulActionSet = actionSet
+        };
+        vr::VRInput()->UpdateActionState(&activeActionSet, sizeof(activeActionSet), 1);
+    }
+
+    bool OpenVRInterface::getHandTransform(vr::ETrackedControllerRole role, Transform& t) {
+        vr::InputPoseActionData_t pose;
+
+        auto retVal = vr::VRInput()->GetPoseActionDataForNextFrame(role == vr::TrackedControllerRole_LeftHand ? leftHand : rightHand, vr::TrackingUniverseStanding, &pose, sizeof(pose), vr::k_ulInvalidInputValueHandle);
+
+        if (retVal != vr::VRInputError_None)
+            return false;
+
+        glm::mat4 matrix = toMat4(pose.pose.mDeviceToAbsoluteTracking);
+
+        t.position = getMatrixTranslation(matrix);
+        t.rotation = getMatrixRotation(matrix);
+
+        return true;
+    }
+
+    glm::mat4 OpenVRInterface::getHeadTransform() {
+        vr::TrackedDevicePose_t hmdPose;
+        system->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseStanding, 0.0f, &hmdPose, 1);
+        return toMat4(hmdPose.mDeviceToAbsoluteTracking);
+    }
+
+    glm::vec2 OpenVRInterface::getLocomotionInput() {
+        vr::InputAnalogActionData_t data;
+        vr::VRInput()->GetAnalogActionData(movementAction, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
+        return glm::vec2(data.x, data.y);
+    }
+
+    bool OpenVRInterface::getJumpInput() {
+        vr::InputDigitalActionData_t data;
+        vr::VRInput()->GetDigitalActionData(jumpAction, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
+
+        return data.bChanged && data.bActive;
+    }
+
+    bool OpenVRInterface::getSprintInput() {
+        vr::InputDigitalActionData_t data;
+        vr::VRInput()->GetDigitalActionData(sprintAction, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
+
+        return data.bState;
+    }
+
+    InputActionHandle OpenVRInterface::getActionHandle(std::string actionPath) {
+        vr::VRActionHandle_t handle;
+        vr::VRInput()->GetActionHandle(actionPath.c_str(), &handle);
+
+        return handle;
+    }
+
+    bool OpenVRInterface::getActionHeld(InputActionHandle handle) {
+        vr::InputDigitalActionData_t data;
+        vr::VRInput()->GetDigitalActionData(handle, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
+
+        return data.bState;
+    }
+
+    bool OpenVRInterface::getActionPressed(InputActionHandle handle) {
+        vr::InputDigitalActionData_t data;
+        vr::VRInput()->GetDigitalActionData(handle, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
+
+        return data.bState && data.bChanged;
+    }
+
+    bool OpenVRInterface::getActionReleased(InputActionHandle handle) {
+        vr::InputDigitalActionData_t data;
+        vr::VRInput()->GetDigitalActionData(handle, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
+
+        return !data.bState && data.bChanged;
+    }
+}
--- ExperimentalEngine/OpenVRInterface.hpp
+++ ExperimentalEngine/OpenVRInterface.hpp
@@ -8,6 +8,7 @@
 #include <glm/gtx/matrix_decompose.hpp>
 #include <filesystem>
 #include <SDL2/SDL_filesystem.h>
+#include "Fatal.hpp"
 #include "MatUtil.hpp"
 #include "Transform.hpp"
 #include "IVRInterface.hpp"
@@ -28,78 +29,14 @@ namespace worlds {
         vr::VRActionHandle_t jumpAction;
         vr::VRActionSetHandle_t actionSet;
 
-        void checkErr(vr::EVRInputError err) {
-            if (err != vr::VRInputError_None) {
-                logErr("VR Input Error: %i", err);
-            }
-        }
+        void checkErr(vr::EVRInputError err);
     public:
-        void init() {
-            vr::EVRInitError eError = vr::VRInitError_None;
-            system = vr::VR_Init(&eError, vr::VRApplication_Scene);
+        void init();
 
-            if (eError != vr::VRInitError_None) {
-                //sprintf_s(buf, sizeof(buf), "Unable to init VR runtime: %s", vr::VR_GetVRInitErrorAsEnglishDescription(eError));
-                std::string errMsg = std::string{"Unable to init VR runtime: "} + vr::VR_GetVRInitErrorAsEnglishDescription(eError);
-                SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "VR_Init Failed", errMsg.c_str(), NULL);
-            }
-            
-            const char* basePath = SDL_GetBasePath();
-            
+        std::vector<std::string> getVulkanInstanceExtensions();
+        std::vector<std::string> getVulkanDeviceExtensions(VkPhysicalDevice physDevice);
 
-            std::string actionsPathStr = basePath + std::string("EEData") + PATH_SEP + "actions.json";
-            const char* actionsPath = actionsPathStr.c_str();
-            logMsg("Using %s as actionsPath", actionsPath);
-            auto vrInput = vr::VRInput();
-            vrInput->SetActionManifestPath(actionsPath);
-            checkErr(vrInput->GetActionHandle("/actions/main/in/Movement", &movementAction));
-            checkErr(vrInput->GetActionHandle("/actions/main/in/LeftHand", &leftHand));
-            checkErr(vrInput->GetActionHandle("/actions/main/in/RightHand", &rightHand));
-            checkErr(vrInput->GetActionHandle("/actions/main/in/Sprint", &sprintAction));
-            checkErr(vrInput->GetActionHandle("/actions/main/in/Jump", &jumpAction));
-
-            checkErr(vrInput->GetActionSetHandle("/actions/main", &actionSet));
-        }
-
-        std::vector<std::string> getVulkanInstanceExtensions() {
-            uint32_t extCharCount = vr::VRCompositor()->GetVulkanInstanceExtensionsRequired(nullptr, 0);
-
-            std::string str((size_t)extCharCount, 'Z');
-
-            vr::VRCompositor()->GetVulkanInstanceExtensionsRequired(str.data(), (uint32_t)str.size());
-
-            std::vector<std::string> extensions;
-
-            std::istringstream stringStream(str.substr(0, extCharCount));
-            std::string currExt;
-            while (std::getline(stringStream, currExt, ' ')) {
-                extensions.push_back(currExt);
-            }
-
-            return extensions;
-        }
-
-        std::vector<std::string> getVulkanDeviceExtensions(VkPhysicalDevice physDevice) {
-            uint32_t extCharCount = vr::VRCompositor()->GetVulkanDeviceExtensionsRequired(physDevice, nullptr, 0);
-
-            std::string str((size_t)extCharCount, 'Z');
-
-            vr::VRCompositor()->GetVulkanDeviceExtensionsRequired(physDevice, str.data(), (uint32_t)str.size());
-
-            std::vector<std::string> extensions;
-
-            std::istringstream stringStream(str);
-            std::string currExt;
-            while (std::getline(stringStream, currExt, ' ')) {
-                extensions.push_back(currExt);
-            }
-
-            return extensions;
-        }
-
-        void getRenderResolution(uint32_t* x, uint32_t* y) {
-            system->GetRecommendedRenderTargetSize(x, y);
-        }
+        void getRenderResolution(uint32_t* x, uint32_t* y);
 
         glm::mat4 toMat4(vr::HmdMatrix34_t mat) {
             return glm::mat4(
@@ -121,103 +58,18 @@ namespace worlds {
             return toMat4(system->GetEyeToHeadTransform(eye));
         }
 
-        void ComposeProjection(float fLeft, float fRight, float fTop, float fBottom, float zNear, float zFar, glm::mat4& p) {
-            float idx = 1.0f / (fRight - fLeft);
-            float idy = 1.0f / (fBottom - fTop);
-            float sx = fRight + fLeft;
-            float sy = fBottom + fTop;
 
-            p[0][0] = 2 * idx; p[1][0] = 0;       p[2][0] = sx * idx;    p[3][0] = 0;
-            p[0][1] = 0;       p[1][1] = 2 * idy; p[2][1] = sy * idy;    p[3][1] = 0;
-            p[0][2] = 0;       p[1][2] = 0;       p[2][2] = 0.0f; p[3][2] = zNear;
-            p[0][3] = 0;       p[1][3] = 0;       p[2][3] = -1.0f;       p[3][3] = 0;
-        }
+        glm::mat4 getProjMat(vr::EVREye eye, float near, float far);
+        void updateInput() override;
+        bool getHandTransform(vr::ETrackedControllerRole role, Transform& t);
+        glm::mat4 getHeadTransform() override;
+        glm::vec2 getLocomotionInput() override;
 
-        glm::mat4 getProjMat(vr::EVREye eye, float near, float far) {
-            float left, right, top, bottom;
-            system->GetProjectionRaw(eye, &left, &right, &top, &bottom);
-
-            glm::mat4 m;
-
-            ComposeProjection(left, right, top, bottom, near, far, m);
-            return m;
-        }
-
-        void updateInput() override {
-            vr::VRActiveActionSet_t activeActionSet{
-                .ulActionSet = actionSet
-            };
-            vr::VRInput()->UpdateActionState(&activeActionSet, sizeof(activeActionSet), 1);
-        }
-
-        bool getHandTransform(vr::ETrackedControllerRole role, Transform& t) {
-            vr::InputPoseActionData_t pose;
-
-            auto retVal = vr::VRInput()->GetPoseActionDataForNextFrame(role == vr::TrackedControllerRole_LeftHand ? leftHand : rightHand, vr::TrackingUniverseStanding, &pose, sizeof(pose), vr::k_ulInvalidInputValueHandle);
-
-            if (retVal != vr::VRInputError_None)
-                return false;
-
-            glm::mat4 matrix = toMat4(pose.pose.mDeviceToAbsoluteTracking);
-
-            t.position = getMatrixTranslation(matrix);
-            t.rotation = getMatrixRotation(matrix);
-
-            return true;
-        }
-
-        glm::mat4 getHeadTransform() override {
-            vr::TrackedDevicePose_t hmdPose;
-            system->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseStanding, 0.0f, &hmdPose, 1);
-            return toMat4(hmdPose.mDeviceToAbsoluteTracking);
-        }
-
-        glm::vec2 getLocomotionInput() override {
-            vr::InputAnalogActionData_t data;
-            vr::VRInput()->GetAnalogActionData(movementAction, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
-            return glm::vec2(data.x, data.y);
-        }
-
-        bool getJumpInput() override {
-            vr::InputDigitalActionData_t data;
-            vr::VRInput()->GetDigitalActionData(jumpAction, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
-
-            return data.bChanged && data.bActive;
-        }
-
-        bool getSprintInput() override {
-            vr::InputDigitalActionData_t data;
-            vr::VRInput()->GetDigitalActionData(sprintAction, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
-
-            return data.bState;
-        }
-
-        InputActionHandle getActionHandle(std::string actionPath) override {
-            vr::VRActionHandle_t handle;
-            vr::VRInput()->GetActionHandle(actionPath.c_str(), &handle);
-
-            return handle;
-        }
-
-        bool getActionHeld(InputActionHandle handle) override {
-            vr::InputDigitalActionData_t data;
-            vr::VRInput()->GetDigitalActionData(handle, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
-
-            return data.bState;
-        }
-
-        bool getActionPressed(InputActionHandle handle) override {
-            vr::InputDigitalActionData_t data;
-            vr::VRInput()->GetDigitalActionData(handle, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
-
-            return data.bState && data.bChanged;
-        }
-
-        bool getActionReleased(InputActionHandle handle) override {
-            vr::InputDigitalActionData_t data;
-            vr::VRInput()->GetDigitalActionData(handle, &data, sizeof(data), vr::k_ulInvalidInputValueHandle);
-
-            return !data.bState && data.bChanged;
-        }
+        bool getJumpInput() override;
+        bool getSprintInput() override;
+        InputActionHandle getActionHandle(std::string actionPath) override;
+        bool getActionHeld(InputActionHandle handle) override;
+        bool getActionPressed(InputActionHandle handle) override;
+        bool getActionReleased(InputActionHandle handle) override;
     };
 }
