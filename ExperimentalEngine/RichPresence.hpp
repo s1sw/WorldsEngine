d1c0b293fc2af8ec980b08085a3adc30b6d54bea
Someone Somewhere <someone167@pm.me>
2020-12-07 23:02:35 +0000

    move from discord rpc to discord sdk

--- ExperimentalEngine/RichPresence.hpp
+++ ExperimentalEngine/RichPresence.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+namespace discord {
+    class Core;
+}
+
 namespace worlds {
+    extern discord::Core* discordCore;
     void initRichPresence(EngineInterfaces interfaces);
     void tickRichPresence();
     void shutdownRichPresence();
