7c0d77120d14d57ab8cf2f4ea9fc073a252a473d
Someone Somewhere <someonesomewhere167@protonmail.com>
2020-12-09 13:45:38 +0000

    component metadata change prep
    Former-commit-id: e33400c6d09b449ecbc7f14d68a7c66b8ee6aedc

--- ExperimentalEngine/ComponentMetadata.hpp
+++ ExperimentalEngine/ComponentMetadata.hpp
@@ -19,10 +19,30 @@ namespace worlds {
 
     class ComponentMetadataManager {
     public:
+        using str_hash_type = entt::hashed_string::hash_type;
+        static std::unordered_map<str_hash_type, ComponentMetadata> strMdata;
         static std::unordered_map<ENTT_ID_TYPE, ComponentMetadata> metadata;
 
         static void registerMetadata(ENTT_ID_TYPE typeId, ComponentMetadata metadata) {
             ComponentMetadataManager::metadata.insert({ typeId, metadata });
+        }
+
+        template <typename T>
+        static void registerEx(
+                std::string name, 
+                bool showInInspector,
+                EditComponentFuncPtr editFuncPtr,
+                AddComponentFuncPtr addFuncPtr,
+                CloneComponentFuncPtr cloneFuncPtr) {
+            registerMetadata(entt::type_info<T>::id(),
+                ComponentMetadata {
+                    name,
+                    showInInspector,
+                    entt::type_info<T>::id(),
+                    editFuncPtr,
+                    addFuncPtr,
+                    cloneFuncPtr
+            });
         }
     };
 }
