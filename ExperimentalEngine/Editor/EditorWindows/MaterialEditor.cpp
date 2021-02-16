0a5b9b2cbc778cc63589dd2831ac2f30423abe5a
Someone Somewhere <someone167@pm.me>
2021-02-16 10:55:28 +0000

    Material editor changes
    Former-commit-id: 174c61ac13404ba4c3ab48019cc1255039ea8f38

--- ExperimentalEngine/Editor/EditorWindows/MaterialEditor.cpp
+++ ExperimentalEngine/Editor/EditorWindows/MaterialEditor.cpp
@@ -13,6 +13,7 @@ namespace worlds {
         AssetID metalMap = ~0u;
         AssetID roughMap = ~0u;
         AssetID heightMap = ~0u;
+        AssetID pbrMap = ~0u;
         glm::vec3 albedoColor = glm::vec3{ 1.0f, 1.0f, 1.0f };
         glm::vec3 emissiveColor = glm::vec3{ 0.0f, 0.0f, 0.0f };
         float metallic = 0.0f;
@@ -21,6 +22,7 @@ namespace worlds {
         float heightmapScale = 0.0f;
         bool cullOff = false;
         bool wireframe = false;
+        bool usePBRMap = false;
     };
 
     void getAssetId(const sajson::value& obj, const char* key, AssetID& aid) {
@@ -53,8 +55,58 @@ namespace worlds {
     }
 
     void assetButton(AssetID& id, const char* title) {
-        bool open = ImGui::Button(title);
+        std::string buttonLabel = "Change##";
+        buttonLabel += title;
+        bool open = ImGui::Button(buttonLabel.c_str());
         selectAssetPopup(title, id, open);
+    }
+
+    void saveMaterial(EditableMaterial& mat, AssetID matId) {
+        std::string j = "{\n";
+
+        j += getJson("albedoPath", g_assetDB.getAssetPath(mat.albedo));
+
+        if (!mat.usePBRMap) {
+            if (mat.roughMap != ~0u)
+                j += ",\n" + getJson("roughMapPath", g_assetDB.getAssetPath(mat.roughMap));
+
+            if (mat.metalMap != ~0u)
+                j += ",\n" + getJson("metalMapPath", g_assetDB.getAssetPath(mat.metalMap));
+        } else {
+            if (mat.pbrMap != ~0u)
+                j += ",\n" + getJson("pbrMapPath", g_assetDB.getAssetPath(mat.pbrMap));
+        }
+
+        if (mat.heightMap != ~0u) {
+            j += ",\n" + getJson("heightmapPath", g_assetDB.getAssetPath(mat.heightMap));
+            j += ",\n" + getJson("heightmapScale", mat.heightmapScale);
+        }
+
+        j += ",\n" + getJson("metallic", mat.metallic);
+        j += ",\n" + getJson("roughness", mat.roughness);
+        j += ",\n" + getJson("albedoColor", mat.albedoColor);
+
+        if (glm::any(glm::greaterThan(mat.emissiveColor, glm::vec3(0.0f)))) {
+            j += ",\n" + getJson("emissiveColor", mat.emissiveColor);
+        }
+
+        if (mat.alphaCutoff != 0.0f)
+            j += ",\n" + getJson("alphaCutoff", mat.alphaCutoff);
+
+        if (mat.cullOff)
+            j += ",\n" + getJson("cullOff", 1);
+
+        if (mat.wireframe)
+            j += ",\n" + getJson("wireframe", 1);
+
+
+        j += "\n}\n";
+
+        logMsg("%s", j.c_str());
+
+        auto* file = g_assetDB.openAssetFileWrite(matId);
+        PHYSFS_writeBytes(file, j.data(), j.size());
+        PHYSFS_close(file);
     }
 
     void MaterialEditor::draw(entt::registry&) {
@@ -65,7 +117,7 @@ namespace worlds {
         if (ImGui::Begin("Material Editor", &active)) {
             if (materialId != ~0u) {
                 if (needsReload) {
-                    PHYSFS_File* f = g_assetDB.openAssetFileRead(materialId);
+                    auto* f = g_assetDB.openAssetFileRead(materialId);
                     size_t fileSize = PHYSFS_fileLength(f);
                     char* buffer = (char*)std::malloc(fileSize);
                     PHYSFS_readBytes(f, buffer, fileSize);
@@ -91,6 +143,11 @@ namespace worlds {
 
                     mat.cullOff = hasKey(root, "cullOff");
                     mat.wireframe = hasKey(root, "wireframe");
+                    mat.usePBRMap = hasKey(root, "pbrMapPath");
+
+                    if (mat.usePBRMap) {
+                        getAssetId(root, "pbrMapPath", mat.pbrMap);
+                    }
 
                     std::free(buffer);
                     needsReload = false;
@@ -105,46 +162,46 @@ namespace worlds {
                 ImGui::ColorEdit3("Albedo Color", &mat.albedoColor.x);
                 ImGui::ColorEdit3("Emissive Color", &mat.emissiveColor.x);
 
+                ImGui::Text("Current albedo path: %s", g_assetDB.getAssetPath(mat.albedo).c_str());
+                ImGui::SameLine();
                 assetButton(mat.albedo, "Albedo");
 
-                if (ImGui::Button("Save")) {
-                    std::string j = "{\n";
-
-                    j += getJson("albedoPath", g_assetDB.getAssetPath(mat.albedo));
-
-                    if (mat.roughMap != ~0u)
-                        j += ",\n" + getJson("roughMapPath", g_assetDB.getAssetPath(mat.roughMap));
-
-                    if (mat.metalMap != ~0u)
-                        j += ",\n" + getJson("metalMapPath", g_assetDB.getAssetPath(mat.metalMap));
-
-                    if (mat.heightMap != ~0u) {
-                        j += ",\n" + getJson("heightmapPath", g_assetDB.getAssetPath(mat.heightMap));
-                        j += ",\n" + getJson("heightmapScale", mat.heightmapScale);
+                ImGui::Checkbox("Use packed PBR map", &mat.usePBRMap);
+                if (mat.usePBRMap) {
+                    if (mat.pbrMap != ~0u) {
+                        ImGui::Text("Current PBR map path: %s", g_assetDB.getAssetPath(mat.pbrMap).c_str());
+                    } else {
+                        ImGui::Text("No PBR map set");
                     }
 
-                    j += ",\n" + getJson("metallic", mat.metallic);
-                    j += ",\n" + getJson("roughness", mat.roughness);
-                    j += ",\n" + getJson("albedoColor", mat.albedoColor);
-
-                    if (glm::any(glm::greaterThan(mat.emissiveColor, glm::vec3(0.0f)))) {
-                        j += ",\n" + getJson("emissiveColor", mat.emissiveColor);
+                    ImGui::SameLine();
+                    assetButton(mat.pbrMap, "PBR Map");
+                } else {
+                    if (mat.metalMap != ~0u) {
+                        ImGui::Text("Current metallic map path: %s", g_assetDB.getAssetPath(mat.metalMap).c_str());
+                    } else {
+                        ImGui::Text("No metallic map set");
                     }
 
-                    if (mat.alphaCutoff != 0.0f)
-                        j += ",\n" + getJson("alphaCutoff", mat.alphaCutoff);
-
-                    if (mat.cullOff)
-                        j += ",\n" + getJson("cullOff", 1);
-
-                    if (mat.wireframe)
-                        j += ",\n" + getJson("wireframe", 1);
+                    ImGui::SameLine();
+                    assetButton(mat.metalMap, "Metal Map");
 
 
-                    j += "\n}\n";
+                    if (mat.roughMap != ~0u) {
+                        ImGui::Text("Current roughness map path: %s", g_assetDB.getAssetPath(mat.roughMap).c_str());
+                    } else {
+                        ImGui::Text("No roughness map set");
+                    }
 
-                    logMsg("%s", j.c_str());
+                    ImGui::SameLine();
+                    assetButton(mat.roughMap, "Rough Map");
                 }
+
+                if (ImGui::Button("Save")) {
+                    saveMaterial(mat, materialId);
+                }
+
+                ImGui::SameLine();
 
                 if (ImGui::Button("Close")) {
                     materialId = ~0u;
