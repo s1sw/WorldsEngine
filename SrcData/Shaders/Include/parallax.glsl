db1b2919279849666f4fd22cc0e9ebb570b1634c
Someone Somewhere <someone167@pm.me>
2021-03-23 18:16:56 +0000

    further 2d hand debug, shader options

--- SrcData/Shaders/Include/parallax.glsl
+++ SrcData/Shaders/Include/parallax.glsl
@@ -1,9 +1,7 @@
 #ifndef PARALLAX_HEADER
 #define PARALLAX_HEADER
 
-vec2 parallaxMapping(vec2 texCoords, vec3 viewDir, sampler2D depthMap, float heightScale) {
-    const float minLayers = 4;
-    const float maxLayers = 32;
+vec2 parallaxMapping(vec2 texCoords, vec3 viewDir, sampler2D depthMap, float heightScale, float minLayers, float maxLayers) {
     float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));
 
     float layerDepth = 1.0 / numLayers;
