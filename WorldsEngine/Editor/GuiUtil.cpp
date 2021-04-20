403ef856d007aa7173e91368476926f8b623b384
Someone Somewhere <someone167@pm.me>
2021-04-20 18:34:00 +0100

    Asset panel improvements
    Former-commit-id: 079d6113fdc0db0711b5ac370e788fb81aafaeea

--- WorldsEngine/Editor/GuiUtil.cpp
+++ WorldsEngine/Editor/GuiUtil.cpp
@@ -8,7 +8,7 @@
 #include <filesystem>
 
 namespace worlds {
-    const char* getIcon(std::string extension) {
+    const char* getIcon(const std::string& extension) {
         if (extension == ".escn" || extension == ".wscn") {
             return (const char*)(ICON_FA_MAP u8" ");
         } else if (extension == ".ogg") {
@@ -19,91 +19,6 @@ namespace worlds {
             return (const char*)(ICON_FA_SHAPES u8" ");
         }
         return "      ";
-    }
-
-    void saveFileModalold(const char* title, std::function<void(const char*)> saveCallback) {
-        ImVec2 popupSize(windowSize.x - 50.0f, windowSize.y - 50.0f);
-        ImGui::SetNextWindowPos(ImGui::GetMainViewport()->Pos + ImVec2((windowSize.x / 2.0f) - (popupSize.x / 2), (windowSize.y / 2.0f) - (popupSize.y / 2)));
-        ImGui::SetNextWindowSize(popupSize);
-
-        if (ImGui::BeginPopupModal(title, nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove)) {
-            std::string* pathPtr = (std::string*)ImGui::GetStateStorage()->GetVoidPtr(ImGui::GetID("savepath"));
-            std::string* saveDir = (std::string*)ImGui::GetStateStorage()->GetVoidPtr(ImGui::GetID("savedir"));
-
-            if (pathPtr == nullptr) {
-                pathPtr = new std::string;
-                ImGui::GetStateStorage()->SetVoidPtr(ImGui::GetID("savepath"), pathPtr);
-            }
-
-            if (saveDir == nullptr) {
-                saveDir = new std::string;
-                ImGui::GetStateStorage()->SetVoidPtr(ImGui::GetID("savedir"), saveDir);
-            }
-
-            ImGui::Text("%s", saveDir->c_str());
-
-            ImGui::BeginChild("Stuffs", ImVec2(ImGui::GetWindowWidth() - 17.0f, ImGui::GetWindowHeight() - 90.0f), true);
-            char** files = PHYSFS_enumerateFiles(saveDir->c_str());
-
-            if (!saveDir->empty()) {
-                ImGui::Text("..");
-                if (ImGui::IsItemClicked()) {
-                    std::filesystem::path p(*saveDir);
-                    *saveDir = p.parent_path().string();
-                }
-            }
-
-            for (char** currFile = files; *currFile != nullptr; currFile++) {
-                PHYSFS_Stat stat;
-                PHYSFS_stat(*currFile, &stat);
-                ImGui::Text("%s%s", *currFile, stat.filetype == PHYSFS_FILETYPE_DIRECTORY ? "/" : "");
-
-                if (ImGui::IsItemClicked()) {
-                    logMsg("clicked filetype of %u", stat.filetype);
-                    if (stat.filetype == PHYSFS_FILETYPE_REGULAR) {
-                        *pathPtr = *currFile;
-                    } else {
-                        *saveDir = *currFile;
-                    }
-                }
-            }
-            ImGui::EndChild();
-
-            std::string titleConfirm = "Confirm##";
-            titleConfirm += title;
-
-            ImGui::PushItemWidth(ImGui::GetWindowWidth() - 109.0f);
-            ImGui::InputText("", pathPtr);
-
-            std::filesystem::path filePath = *saveDir;
-            filePath.append(*pathPtr);
-
-            ImGui::SameLine();
-            if (ImGui::Button("OK") && !pathPtr->empty()) {
-                if (PHYSFS_exists(filePath.string().c_str())) {
-                    ImGui::OpenPopup(titleConfirm.c_str());
-                } else {
-                    saveCallback(filePath.string().c_str());
-                    ImGui::CloseCurrentPopup();
-                }
-            }
-
-            ImGui::SameLine();
-            if (ImGui::Button("Cancel"))
-                ImGui::CloseCurrentPopup();
-
-            if (ImGui::BeginPopup(titleConfirm.c_str())) {
-                ImGui::Text("This file already exists. Do you want to overwrite?");
-
-                if (ImGui::Button("Overwrite")) {
-                    saveCallback(filePath.string().c_str());
-                    ImGui::ClosePopupToLevel(0, true);
-                }
-                ImGui::EndPopup();
-            }
-
-            ImGui::EndPopup();
-        }
     }
 
     void listDirectoryItems(std::string path,
@@ -145,6 +60,8 @@ namespace worlds {
                     onDoubleClick(absPath, stat.filetype);
                 }
             }
+
+            PHYSFS_freeList(files);
     }
 
     void saveFileModal(const char* title, std::function<void(const char*)> saveCallback) {
