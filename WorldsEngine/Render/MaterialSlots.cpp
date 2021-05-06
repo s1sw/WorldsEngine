355bb18ce2d0a7070ef4c81dc92207c67e202067
Someone Somewhere <someone167@pm.me>
2021-05-06 18:29:10 +0100

    Move from sajson to nlohmann::json for material parsing

--- WorldsEngine/Render/MaterialSlots.cpp
+++ WorldsEngine/Render/MaterialSlots.cpp
@@ -2,9 +2,19 @@
 #include "../Core/Engine.hpp"
 #include "tracy/Tracy.hpp"
 #include <physfs.h>
-#include <sajson.h>
 #include <optional>
 #include "../Util/JsonUtil.hpp"
+#include <nlohmann/json.hpp>
+
+namespace glm {
+    void to_json(nlohmann::json& j, const glm::vec3& vec) {
+        j = nlohmann::json{vec.x, vec.y, vec.z};
+    }
+
+    void from_json(const nlohmann::json& j, glm::vec3& vec) {
+        vec = glm::vec3{j[0], j[1], j[2]};
+    }
+}
 
 namespace worlds {
     std::optional<std::string> getString(const sajson::value& obj, const char* key) {
@@ -16,6 +26,14 @@ namespace worlds {
             return std::nullopt;
 
         return obj.get_object_value(idx).as_string();
+    }
+
+    uint32_t MaterialSlots::getTexture(nlohmann::json& j, std::string key) {
+        auto it = j.find(key);
+
+        if (it == j.end()) return ~0u;
+
+        return texSlots.loadOrGet(g_assetDB.addOrGetExisting(it.value()));
     }
 
     void MaterialSlots::parseMaterial(AssetID asset, PackedMaterial& mat, MatExtraData& extraDat) {
@@ -31,126 +49,74 @@ namespace worlds {
         }
 
         size_t fileSize = PHYSFS_fileLength(f);
-        char* buffer = (char*)std::malloc(fileSize);
-        PHYSFS_readBytes(f, buffer, fileSize);
+        std::string str;
+        str.resize(fileSize);
+        // Add a null byte to the end to make a C string
+        PHYSFS_readBytes(f, str.data(), fileSize);
         PHYSFS_close(f);
 
-        const sajson::document& document = sajson::parse(
-            sajson::single_allocation(), sajson::mutable_string_view(fileSize, buffer)
-        );
+        try {
+            auto j = nlohmann::json::parse(str);
 
-        if (!document.is_valid()) {
+            if (j.type() != nlohmann::detail::value_t::object) {
+                SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Invalid material document");
+                return;
+            }
+
+            auto albedoPath = j["albedoPath"];
+
+            mat.setCutoff(j.value("alphaCutoff", 0.0));
+            mat.setFlags(MaterialFlags::None);
+
+            mat.metallic = j["metallic"];
+            mat.roughness = j["roughness"];
+            mat.albedoColor = j["albedoColor"].get<glm::vec3>();
+
+            if (j.find("emissiveColor") != j.end()) {
+                mat.emissiveColor = j["emissiveColor"].get<glm::vec3>();
+
+                // Ignore tiny emmisive values
+                if (glm::compAdd(mat.emissiveColor) < 0.003f) {
+                    mat.emissiveColor = glm::vec3 {0.0f};
+                }
+            } else {
+                mat.emissiveColor = glm::vec3 {0.0f};
+            }
+
+            auto albedoAssetId = g_assetDB.addOrGetExisting(albedoPath);
+
+            uint32_t nMapSlot = getTexture(j, "normalMapPath");
+            uint32_t hMapSlot = getTexture(j, "heightmapPath");
+
+            mat.albedoTexIdx = texSlots.loadOrGet(albedoAssetId);
+            mat.normalTexIdx = nMapSlot;
+            mat.heightmapTexIdx = hMapSlot;
+
+            auto metalMap = getTexture(j, "metalMapPath");
+            auto roughMap = getTexture(j, "roughMapPath");
+            auto aoMap = getTexture(j, "aoMapPath");
+            auto pbrMap = getTexture(j, "pbrMapPath");
+
+            if (pbrMap != ~0u) {
+                mat.roughTexIdx = pbrMap;
+                mat.setFlags(mat.getFlags() | MaterialFlags::UsePackedPBR);
+            } else {
+                mat.metalTexIdx = metalMap;
+                mat.roughTexIdx = roughMap;
+                mat.aoTexIdx = aoMap;
+            }
+
+            mat.heightmapScale = j.value("heightmapScale", 0.0);
+
+            auto cullOffIt = j.find("cullOff");
+            extraDat.noCull = cullOffIt != j.end();
+
+            auto wireframeIt = j.find("wireframe");
+            extraDat.wireframe = wireframeIt != j.end();
+        } catch(nlohmann::detail::exception& ex) {
             std::string path = g_assetDB.getAssetPath(asset);
-            logErr(WELogCategoryRender, "Invalid material document %s (parse failed)", path.c_str());
-            std::free(buffer);
-            return;
+            logErr(WELogCategoryRender, "Invalid material document %s (%s)", path.c_str(), ex.what());
         }
-
-        const auto& root = document.get_root();
-
-        if (root.get_type() != sajson::TYPE_OBJECT) {
-            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Invalid material document");
-            std::free(buffer);
-            return;
-        }
-
-        auto rootLength = root.get_length();
-        auto albedoPathIdx = root.find_object_key(sajson::string("albedoPath", 10));
-        auto metallicIdx = root.find_object_key(sajson::string("metallic", 8));
-        auto roughnessIdx = root.find_object_key(sajson::string("roughness", 9));
-        auto albedoColorIdx = root.find_object_key(sajson::string("albedoColor", 11));
-        auto alphaCutoffIdx = root.find_object_key(sajson::string("alphaCutoff", 11));
-        auto heightmapScaleIdx = root.find_object_key(sajson::string("heightmapScale", 14));
-        auto emissiveColorIdx = root.find_object_key(sajson::string("emissiveColor", 13));
-
-        auto albedoPath = root.get_object_value(albedoPathIdx).as_string();
-
-        float alphaCutoff = 0.0f;
-        if (alphaCutoffIdx != rootLength)
-            alphaCutoff = root.get_object_value(alphaCutoffIdx).get_double_value();
-
-        mat.setCutoff(alphaCutoff);
-        mat.setFlags(MaterialFlags::None);
-
-        mat.metallic = (float)root.get_object_value(metallicIdx).get_double_value();
-        mat.roughness = (float)root.get_object_value(roughnessIdx).get_double_value();
-        const auto& albedoColorArr = root.get_object_value(albedoColorIdx);
-
-        glm::vec3 albedoColor{
-            albedoColorArr.get_array_element(0).get_double_value(),
-            albedoColorArr.get_array_element(1).get_double_value(),
-            albedoColorArr.get_array_element(2).get_double_value()
-        };
-
-        mat.albedoColor = albedoColor;
-
-        if (emissiveColorIdx != rootLength) {
-            const auto& emissiveColorArr = root.get_object_value(emissiveColorIdx);
-
-            mat.emissiveColor = glm::vec3 {
-               emissiveColorArr.get_array_element(0).get_double_value(), 
-               emissiveColorArr.get_array_element(1).get_double_value(), 
-               emissiveColorArr.get_array_element(2).get_double_value()
-            };
-        } else {
-            mat.emissiveColor = glm::vec3 {0.0f};
-        }
-
-        auto albedoAssetId = g_assetDB.addOrGetExisting(albedoPath);
-
-        uint32_t nMapSlot = ~0u;
-        auto normalMap = getString(root, "normalMapPath");
-        if (normalMap) {
-            auto normalMapId = g_assetDB.addOrGetExisting(*normalMap);
-            nMapSlot = texSlots.loadOrGet(normalMapId);
-        }
-
-        uint32_t hMapSlot = ~0u;
-        auto heightmap = getString(root, "heightmapPath");
-        if (heightmap) {
-            auto heightMapId = g_assetDB.addOrGetExisting(*heightmap);
-            hMapSlot = texSlots.loadOrGet(heightMapId);
-        }
-
-        mat.albedoTexIdx = texSlots.loadOrGet(albedoAssetId);
-        mat.normalTexIdx = nMapSlot;
-        mat.heightmapTexIdx = hMapSlot;
-
-        mat.metalTexIdx = ~0u;
-        mat.roughTexIdx = ~0u;
-        mat.aoTexIdx = ~0u;
-
-        auto metalMap = getString(root, "metalMapPath");
-        auto roughMap = getString(root, "roughMapPath");
-        auto aoMap = getString(root, "aoMapPath");
-        auto pbrMap = getString(root, "pbrMapPath");
-
-        if (metalMap)
-            mat.metalTexIdx = texSlots.loadOrGet(g_assetDB.addOrGetExisting(*metalMap));
-
-        if (roughMap)
-            mat.roughTexIdx = texSlots.loadOrGet(g_assetDB.addOrGetExisting(*roughMap));
-
-        if (aoMap)
-            mat.aoTexIdx = texSlots.loadOrGet(g_assetDB.addOrGetExisting(*aoMap));
-
-        if (pbrMap) {
-            mat.roughTexIdx = texSlots.loadOrGet(g_assetDB.addOrGetExisting(*pbrMap));
-            mat.setFlags(mat.getFlags() | MaterialFlags::UsePackedPBR);
-        }
-
-        float heightmapScale = 0.0f;
-        if (heightmapScaleIdx != rootLength)
-            heightmapScale = root.get_object_value(heightmapScaleIdx).get_double_value();
-        mat.heightmapScale = heightmapScale;
-        
-        auto bfCullOffIdx = root.find_object_key(sajson::string("cullOff", 7));
-        extraDat.noCull = bfCullOffIdx != rootLength;
-
-        auto wireframeIdx = root.find_object_key(sajson::string("wireframe", 9));
-        extraDat.wireframe = wireframeIdx != rootLength;
-
-        std::free(buffer);
     }
 
     uint32_t MaterialSlots::load(AssetID asset) {
