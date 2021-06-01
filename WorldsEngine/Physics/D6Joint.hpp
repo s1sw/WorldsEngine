b3adcc03a63ed10fdd9a6915d583d1201cebf2a2
Someone Somewhere <someone167@pm.me>
2021-06-01 15:28:09 +0100

    Additions to D6 joint API
    Former-commit-id: 8f851404cb969237dea05373b732930eb7bf0ce7

--- WorldsEngine/Physics/D6Joint.hpp
+++ WorldsEngine/Physics/D6Joint.hpp
@@ -7,14 +7,26 @@ namespace physx {
 }
 
 namespace worlds {
+    enum class D6Motion {
+        Locked,
+        Limited,
+        Free
+    };
+
+    enum class D6Axis {
+    };
+
     struct D6Joint {
         D6Joint();
         D6Joint(const D6Joint&) = delete;
         D6Joint(D6Joint&& other) noexcept;
         void operator=(D6Joint&& other);
         physx::PxD6Joint* pxJoint;
+        bool reverseJoint = false;
 
         void setTarget(entt::entity newTargetEnt, entt::registry& reg);
+        void setAllLinearMotion(D6Motion motion);
+        void setAllAngularMotion(D6Motion motion);
         entt::entity getTarget();
 
         ~D6Joint();
