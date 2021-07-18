bfd56844fad100890f5a5c7ffe4c54e1d89aebf9
Someone Somewhere <sackboydj167@gmail.com>
2021-07-19 00:10:52 +0100

    Added seperate game DLL
    Former-commit-id: a6563cdd54d82dbe1dfcd89f03207b60df9d9804

---
+++ Game/TestSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WorldsEngine;
+
+namespace Game
+{
+    public class TestSystem : ISystem
+    {
+        private Registry registry;
+
+        public TestSystem(Registry registry)
+        {
+            this.registry = registry;
+        }
+
+        public void OnUpdate(float deltaTime)
+        {
+            if (ImGui.Begin("WOOOOOO"))
+            {
+                ImGui.End();
+            }
+        }
+    }
+}
---
+++ WorldsEngineManaged/SystemAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace WorldsEngine
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SystemAttribute : Attribute
+    {
+    }
+}
