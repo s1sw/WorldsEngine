9e2f55c2cf6ba641e3c041728c005bac82dbef8c
Someone Somewhere <someone167@pm.me>
2021-09-05 19:20:08 +0100

    Win32 splash screen

---
+++ WorldsEngine/Core/SplashScreenImpls/SplashScreenWin32.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "ISplashScreen.hpp"
+
+struct HWND__;
+typedef struct HWND__* HWND;
+typedef unsigned long long WPARAM;
+typedef long long LPARAM;
+typedef __int64 LONG_PTR;
+typedef LONG_PTR LRESULT;
+
+namespace worlds {
+    class SplashScreenImplWin32 : public ISplashScreen {
+    public:
+        SplashScreenImplWin32(bool small);
+        void changeOverlay(const char*) override;
+        ~SplashScreenImplWin32();
+    private:
+        void eventLoop();
+        // Avoid pulling in windows.h
+        // Yes this file is only included in one place but that
+        // file already has so many includes it's ridiculous
+        struct State;
+        State* s;
+
+        static LRESULT WndProc(HWND, unsigned int, WPARAM, LPARAM);
+    };
+}
--- WorldsEngine/Core/SplashScreenImpls/SplashScreenX11.cpp
+++ WorldsEngine/Core/SplashScreenImpls/SplashScreenX11.cpp
@@ -1,3 +1,11 @@
+// this took 6 hours to get to properly display the splash screen. insanity.
+// right now, it doesn't display overlays so you don't get the text saying
+// what's currently loading but honestly i value my mental health more than
+// some little "loading renderer!!" text
+// 
+// for the sake of my sanity, i'm staying far away from the legacy infested
+// spiderweb that is XCB for the forseeable future.
+#ifdef __linux__
 #include "SplashScreenX11.hpp"
 #include "Core/Log.hpp"
 #include "SDL_filesystem.h"
@@ -16,21 +24,20 @@ namespace worlds {
         return reply;
     }
 
-    static xcb_format_t *
-        find_format (xcb_connection_t * c, uint8_t depth, uint8_t bpp) {
-            const xcb_setup_t *setup = xcb_get_setup(c);
-            xcb_format_t *fmt = xcb_setup_pixmap_formats(setup);
-            xcb_format_t *fmtend = fmt + xcb_setup_pixmap_formats_length(setup);
-            for(; fmt != fmtend; ++fmt) {
-                printf("fmt %p has pad %d depth %d, bpp %d\n",
-                        fmt,fmt->scanline_pad, fmt->depth,fmt->bits_per_pixel);
-                if((fmt->depth == depth) && (fmt->bits_per_pixel == bpp)) {
-                    return fmt;
-                }
+    // taken from http://vincentsanders.blogspot.com/2010/04/xcb-programming-is-hard.html
+    xcb_format_t* findFormat(xcb_connection_t* c, uint8_t depth, uint8_t bpp) {
+        const xcb_setup_t* setup = xcb_get_setup(c);
+        xcb_format_t* fmt = xcb_setup_pixmap_formats(setup);
+        xcb_format_t* fmtend = fmt + xcb_setup_pixmap_formats_length(setup);
+        for (; fmt != fmtend; ++fmt) {
+            if ((fmt->depth == depth) && (fmt->bits_per_pixel == bpp)) {
+                return fmt;
             }
-            return 0;
         }
+        return 0;
+    }
 
+    // this is not a place of honour. nothing valued is here. what is here is dangerous and repulsive to us.
     xcb_pixmap_t loadDataFileToPixmap(xcb_connection_t* connection, uint8_t depth, xcb_window_t window, const char* fileName) {
         const char* basePath = SDL_GetBasePath();
 
@@ -41,15 +48,6 @@ namespace worlds {
         strcat(buf, "EngineData/");
         strcat(buf, fileName);
         logMsg("Loading splash screen from %s", buf);
-
-        //FILE* f = fopen(buf, "rb");
-        //fseek(f, 0, SEEK_END);
-        //size_t fileLen = ftell(f);
-        //fseek(f, 0, SEEK_SET);
-
-        //unsigned char* fileData = (unsigned char*)malloc(fileLen);
-        //fread(fileData, fileLen, 1, f);
-        //fclose(f);
 
         int width, height, channels;
         unsigned char* imgData = stbi_load(buf, &width, &height, &channels, 4);
@@ -62,16 +60,16 @@ namespace worlds {
         }
 
         const xcb_setup_t* setup = xcb_get_setup(connection);
-        const xcb_format_t* format = find_format(connection, 24, 32);
+        const xcb_format_t* format = findFormat(connection, 24, 32);
 
         xcb_image_t* img = xcb_image_create(
-                width, height,
-                XCB_IMAGE_FORMAT_Z_PIXMAP,
-                format->scanline_pad, format->depth, format->bits_per_pixel, 0,
-                (xcb_image_order_t)setup->image_byte_order, XCB_IMAGE_ORDER_LSB_FIRST,
-                imgData,
-                width * height * 4,
-                imgData);
+            width, height,
+            XCB_IMAGE_FORMAT_Z_PIXMAP,
+            format->scanline_pad, format->depth, format->bits_per_pixel, 0,
+            (xcb_image_order_t)setup->image_byte_order, XCB_IMAGE_ORDER_LSB_FIRST,
+            imgData,
+            width * height * 4,
+            imgData);
         xcb_image_t* nativeImg = xcb_image_native(connection, img, 1);
 
         if (nativeImg != img) {
@@ -80,13 +78,9 @@ namespace worlds {
 
         xcb_pixmap_t pixmap = xcb_generate_id(connection);
 
-        logMsg("create pixmap seq: %i", xcb_create_pixmap(connection, depth, pixmap, window, width, height).sequence);
-
         xcb_gcontext_t pixmapGc = xcb_generate_id(connection);
-        logMsg("xcb_create_gc seq: %i", xcb_create_gc(connection, pixmapGc, pixmap, 0, nullptr).sequence);
         xcb_flush(connection);
 
-        logMsg("xcb_image_put seq: %i", xcb_image_put(connection, pixmap, pixmapGc, nativeImg, 0, 0, 0).sequence);
         xcb_flush(connection);
 
         xcb_free_gc(connection, pixmapGc);
@@ -107,27 +101,28 @@ namespace worlds {
 
         window = xcb_generate_id(connection);
 
-        uint32_t windowMask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+        // We want to receive the exposure event
+        uint32_t windowMask = XCB_CW_EVENT_MASK;
         uint32_t windowValues[] = {
-            screen->black_pixel,
             XCB_EVENT_MASK_EXPOSURE
         };
 
         xcb_create_window(
-                connection,
-                XCB_COPY_FROM_PARENT,
-                window,
-                screen->root,
-                0, 0,
-                small ? 460 : 800, small ? 215 : 600,
-                0,
-                XCB_WINDOW_CLASS_INPUT_OUTPUT,
-                screen->root_visual,
-                windowMask, windowValues
-                );
+            connection,
+            XCB_COPY_FROM_PARENT,
+            window,
+            screen->root,
+            0, 0,
+            small ? 460 : 800, small ? 215 : 600,
+            0,
+            XCB_WINDOW_CLASS_INPUT_OUTPUT,
+            screen->root_visual,
+            windowMask, windowValues
+        );
 
         background = loadDataFileToPixmap(connection, screen->root_depth, window, "splash.png");
 
+        // Motif hint to remove decoration
         struct MotifWMHints {
             uint32_t flags;
             uint32_t functions;
@@ -139,14 +134,15 @@ namespace worlds {
         MotifWMHints hints = { 0 };
         hints.flags = 2;
 
-        xcb_ewmh_connection_t ewmhConnection;
-        xcb_ewmh_init_atoms_replies(&ewmhConnection, xcb_ewmh_init_atoms(connection, &ewmhConnection), nullptr);
-
-        xcb_intern_atom_reply_t* atomReply = getAtom(connection, "_MOTIF_WM_HINTS");
-        if (atomReply) {
-            xcb_change_property(connection, XCB_PROP_MODE_REPLACE, window, atomReply->atom, atomReply->atom, 32, 5, &hints);
+        xcb_intern_atom_reply_t* motifAtomReply = getAtom(connection, "_MOTIF_WM_HINTS");
+        if (motifAtomReply) {
+            xcb_change_property(connection, XCB_PROP_MODE_REPLACE, window, motifAtomReply->atom, motifAtomReply->atom, 32, 5, &hints);
             free(atomReply);
         }
+
+        // EWMH hint to set the window type to a splash screen
+        xcb_ewmh_connection_t ewmhConnection;
+        xcb_ewmh_init_atoms_replies(&ewmhConnection, xcb_ewmh_init_atoms(connection, &ewmhConnection), nullptr);
 
         xcb_ewmh_set_wm_window_type(&ewmhConnection, window, 1, &ewmhConnection._NET_WM_WINDOW_TYPE_SPLASH);
         xcb_flush(connection);
@@ -159,39 +155,34 @@ namespace worlds {
         uint32_t value[] = { screen->black_pixel };
 
         xcb_create_gc(connection, graphicsContext, window, mask, value);
-
-        xcb_rectangle_t rect = { 0, 0, 800, 600 };
-        xcb_poly_fill_rectangle(connection, window, graphicsContext, 1, &rect);
-
         xcb_flush(connection);
 
-        thread = new std::thread{&SplashScreenImplX11::eventLoop, this};
+        thread = new std::thread{ &SplashScreenImplX11::eventLoop, this };
     }
 
     void SplashScreenImplX11::handleEvent(xcb_generic_event_t* event) {
         xcb_rectangle_t rect = { 0, 0, 800, 600 };
         switch (event->response_type & ~0x80) {
-            case XCB_EXPOSE: {
-                                 xcb_expose_event_t* expose = (xcb_expose_event_t*)event;
-                                 //xcb_poly_fill_rectangle(connection, window, graphicsContext, 1, &rect);
-                                 xcb_copy_area(connection, background, window, graphicsContext,
-                                         0, 0,
-                                         0, 0,
-                                         800, 600);
-                                 xcb_flush (connection);
-                                 break;
-                             }
-            case 0: {
-                        xcb_generic_error_t* error = (xcb_generic_error_t*)event;
-                        logErr("XCB Error: %i (%i:%i, seq %i)", error->error_code, error->minor_code, error->major_code, error->sequence);
-                        if (error->error_code == XCB_VALUE) {
-                            xcb_value_error_t* valError = (xcb_value_error_t*)event;
-                            logErr("value error: %u", valError->bad_value);
-                        }
-                        break;
-                    }
-            default:
-                    break;
+        case XCB_EXPOSE: {
+            xcb_expose_event_t* expose = (xcb_expose_event_t*)event;
+            xcb_copy_area(connection, background, window, graphicsContext,
+                0, 0,
+                0, 0,
+                800, 600);
+            xcb_flush(connection);
+            break;
+        }
+        case 0: {
+            xcb_generic_error_t* error = (xcb_generic_error_t*)event;
+            logErr("XCB Error: %i (%i:%i, seq %i)", error->error_code, error->minor_code, error->major_code, error->sequence);
+            if (error->error_code == XCB_VALUE) {
+                xcb_value_error_t* valError = (xcb_value_error_t*)event;
+                logErr("value error: %u", valError->bad_value);
+            }
+            break;
+        }
+        default:
+            break;
         }
     }
 
@@ -200,7 +191,7 @@ namespace worlds {
             xcb_generic_event_t* event;
             while ((event = xcb_poll_for_event(connection))) {
                 handleEvent(event);
-                free (event);
+                free(event);
             }
         }
         logMsg("splash event loop exiting");
@@ -216,3 +207,4 @@ namespace worlds {
     }
 }
 
+#endif
