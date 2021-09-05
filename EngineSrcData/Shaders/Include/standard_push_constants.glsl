d140dfc4a1fd07af22ab0fef664c2cf382e5b382
Someone Somewhere <someone167@pm.me>
2021-09-06 00:43:04 +0100

    Sorted render misc flag order
    Former-commit-id: a4f03aaa78167362600d8ad8b1397d0539a65406

--- EngineSrcData/Shaders/Include/standard_push_constants.glsl
+++ EngineSrcData/Shaders/Include/standard_push_constants.glsl
@@ -22,18 +22,35 @@ layout(push_constant) uniform PushConstants {
     // 4 - Debug display roughness             (8)
     // 5 - Debug display AO                    (16)
     // 6 - Debug display normal map            (32)
-    // 7 - Lighting only                       (64)
-    // 8 - World space UVs (XY)                (128)
-    // 9 - World space UVs (XZ)                (256)
-    // 10 - World space UVs (ZY)               (512)
-    // 11 - World space UVs (pick with normal) (1024)
-    // 12 - Debug display UVs                  (2048)
-    // 13 - Use cubemap parallax               (4096)
-    // 14 - Debug display shadowmap cascades   (8192)
-    // 15 - Disable shadows                    (16384)
-    // 16 - Debug display albedo               (32768)
+    // 7 - Debug lighting only                 (64)
+	// 8 - Debug display UVs                   (128)
+	// 9 - Debug display shadowmap cascades    (256)
+	// 10 - Debug display albedo               (512)
+    // 11 - World space UVs (XY)               (1024)
+    // 12 - World space UVs (XZ)               (2048)
+    // 13 - World space UVs (ZY)               (4096)
+    // 14 - World space UVs (pick with normal) (8192)
+    // 15 - Use cubemap parallax               (16384)
+    // 16 - Disable shadows                    (32768)
     uint miscFlag;
     uint cubemapIdx;
     //total: 80 bytes
 };
+
+const int DBG_FLAG_NORMALS = 2;
+const int DBG_FLAG_METALLIC = 4;
+const int DBG_FLAG_ROUGHNESS = 8;
+const int DBG_FLAG_AO = 16;
+const int DBG_FLAG_NORMAL_MAP = 32;
+const int DBG_FLAG_LIGHTING_ONLY = 64;
+const int DBG_FLAG_UVS = 128;
+const int DBG_FLAG_SHADOW_CASCADES = 256;
+const int DBG_FLAG_ALBEDO = 512;
+
+const int MISC_FLAG_UV_XY = 1024;
+const int MISC_FLAG_UV_XZ = 2048;
+const int MISC_FLAG_UV_ZY = 4096;
+const int MISC_FLAG_UV_PICK = 8192;
+const int MISC_FLAG_CUBEMAP_PARALLAX = 16384;
+const int MISC_FLAG_DISABLE_SHADOWS = 32768;
 #endif
