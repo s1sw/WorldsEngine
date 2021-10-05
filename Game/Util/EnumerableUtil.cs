c9f28d3f38d66a3342f8a71533794e49b57dbbc5
Someone Somewhere <someone167@pm.me>
2021-10-05 23:28:19 +0100

    Box grip fixes

---
+++ Game/Util/EnumerableUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Util
+{
+    public static class EnumerableUtil
+    {
+        public static bool TryFirst<T>(this IEnumerable<T> seq, out T result)
+        {
+            result = default;
+            foreach (var item in seq)
+            {
+                result = item;
+                return true;
+            }
+            return false;
+        }
+    }
+}
