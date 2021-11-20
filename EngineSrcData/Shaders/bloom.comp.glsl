3e373b172c058b13376aa2f12f18a2bc5865f603
Someone Somewhere <someone167@pm.me>
2021-11-20 21:37:56 +0000

    More work on bloom

--- EngineSrcData/Shaders/bloom.comp.glsl
+++ EngineSrcData/Shaders/bloom.comp.glsl
@@ -5,16 +5,7 @@ layout (binding = 1, rgba16f) uniform writeonly image2D img;
 
 void main() {
     vec2 size = textureSize(mipChain, 0);
-    vec3 accumulator = vec3(0.0);
-
-    //int numTaps = 5;
-    //float weight = 1.0 / float(numTaps);
-    //int startMip = 2;
-    //for (int i = startMip; i < numTaps + startMip; i++) {
-    //    accumulator += textureLod(mipChain, vec2(gl_GlobalInvocationID.xy / size), i).xyz;
-    //}
-    //accumulator *= (1.0 / float(numTaps));
-    accumulator = textureLod(mipChain, (vec2(gl_GlobalInvocationID.xy) + 0.5) / size, 0).xyz;
+    vec3 val = textureLod(mipChain, (vec2(gl_GlobalInvocationID.xy) + 0.5) / size, 0).xyz;
     
-    imageStore(img, ivec2(gl_GlobalInvocationID.xy), vec4(accumulator, 1.0));
+    imageStore(img, ivec2(gl_GlobalInvocationID.xy), vec4(val, 1.0));
 }
