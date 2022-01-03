288d85d97ba3485c51194b32e37ce7cd8904c8e0
Someone Somewhere <someone167@pm.me>
2022-01-03 20:52:25 +0000

    AI disable command
    Former-commit-id: cc2130490d1af1bfd7ff6dcd6caa46f0bc12ec18

---
+++ Game/DebugGlobals.cs
@@ -0,0 +1,15 @@
+﻿using WorldsEngine;
+
+namespace Game
+{
+    internal class DebugGlobals
+    {
+        public static bool AIIgnorePlayer = false;
+
+        [ConsoleCommand("game_toggleAiIgnore", "Toggles AI ignoring the player.")]
+        public static void RegisterCommands(string args)
+        {
+            AIIgnorePlayer = !AIIgnorePlayer;
+        }
+    }
+}
