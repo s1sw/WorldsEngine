cc38d113426131eb504fb52e6a74c60bc999e961
Someone Somewhere <someone167@pm.me>
2022-01-03 20:51:56 +0000

    Add MathsUtil to engine

---
+++ WorldsEngine/Util/MathsUtil.cpp
@@ -0,0 +1,29 @@
+#include "MathsUtil.hpp"
+
+namespace worlds {
+    glm::quat fixupQuat(glm::quat q) {
+        return q * glm::sign(glm::dot(q, glm::quat_identity<float, glm::packed_highp>()));
+    }
+
+    float WrapAngle(float inputAngle) {
+        //The inner % 360 restricts everything to +/- 360
+        //+360 moves negative values to the positive range, and positive ones to > 360
+        //the final % 360 caps everything to 0...360
+        return fmodf((fmodf(inputAngle, 360.0f) + 360.0f), 360.0f);
+    }
+
+    float AngleToErr(float angle) {
+        angle = WrapAngle(angle);
+        if (angle > 180.0f && angle < 360.0f) {
+            angle = 360.0f - angle;
+
+            angle *= -1.0f;
+        }
+
+        return angle;
+    }
+
+    glm::quat safeQuatLookat(glm::vec3 dir, glm::vec3 up, glm::vec3 fallbackUp) {
+        return glm::abs(glm::dot(dir, up)) > 0.999f ? glm::quatLookAt(dir, fallbackUp) : glm::quatLookAt(dir, up);
+    }
+}
