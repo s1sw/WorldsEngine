0614f21cea3ee8513ae1795ec9e53da3071b3344
Someone Somewhere <someone167@pm.me>
2022-01-16 23:14:59 +0000

    D6 joint components can now specify both actors in the joint

--- WorldsEngine/Physics/D6Joint.cpp
+++ WorldsEngine/Physics/D6Joint.cpp
@@ -5,6 +5,7 @@
 #include <entt/entt.hpp>
 #include "PhysicsActor.hpp"
 #include "../Core/Log.hpp"
+#include <Physics/Physics.hpp>
 
 namespace worlds {
     D6Joint::D6Joint() : pxJoint{ nullptr }, thisActor{ nullptr }, targetEntity{ entt::null } {}
@@ -13,12 +14,27 @@ namespace worlds {
         pxJoint = other.pxJoint; other.pxJoint = nullptr;
         thisActor = other.thisActor; other.thisActor = nullptr;
         targetEntity = other.targetEntity; other.targetEntity = entt::null;
+        replaceThis = other.replaceThis; other.replaceThis = entt::null;
+        originalThisActor = other.originalThisActor; other.originalThisActor = nullptr;
     }
 
     void D6Joint::operator=(D6Joint&& other) {
         pxJoint = other.pxJoint; other.pxJoint = nullptr;
         thisActor = other.thisActor; other.thisActor = nullptr;
         targetEntity = other.targetEntity; other.targetEntity = entt::null;
+        replaceThis = other.replaceThis; other.replaceThis = entt::null;
+        originalThisActor = other.originalThisActor; other.originalThisActor = nullptr;
+    }
+
+    physx::PxRigidActor* getSuitablePhysicsActor(entt::entity ent, entt::registry& reg) {
+        auto* pa = reg.try_get<PhysicsActor>(ent);
+        auto* dpa = reg.try_get<DynamicPhysicsActor>(ent);
+
+        if (!(pa || dpa)) {
+            return nullptr;
+        }
+
+        return pa ? pa->actor : dpa->actor;
     }
 
     void D6Joint::setTarget(entt::entity newTargetEnt, entt::registry& reg) {
@@ -31,12 +47,8 @@ namespace worlds {
             return;
         }
 
-        auto other = pa ? pa->actor : dpa->actor;
-
-        if (reverseJoint)
-            pxJoint->setActors(other, thisActor);
-        else
-            pxJoint->setActors(thisActor, other);
+        targetActor = getSuitablePhysicsActor(newTargetEnt, reg);
+        updateJointActors();
     }
 
     physx::PxD6Motion::Enum conv(D6Motion motion) {
@@ -61,6 +73,29 @@ namespace worlds {
         return targetEntity;
     }
 
+    entt::entity D6Joint::getAttached() {
+        return replaceThis;
+    }
+
+    void D6Joint::setAttached(entt::entity ent, entt::registry& reg) {
+        replaceThis = ent;
+
+        if (ent != entt::null) {
+            thisActor = getSuitablePhysicsActor(ent, reg);
+        } else {
+            thisActor = originalThisActor;
+        }
+
+        updateJointActors();
+    }
+
     D6Joint::~D6Joint() {
+    }
+
+    void D6Joint::updateJointActors() {
+        if (reverseJoint)
+            pxJoint->setActors(targetActor, thisActor);
+        else
+            pxJoint->setActors(thisActor, targetActor);
     }
 }
