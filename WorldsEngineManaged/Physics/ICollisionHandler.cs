21c8e2c891d0fcc0b0cf1288257be3ea8bfba829
Someone Somewhere <someone167@pm.me>
2022-02-07 18:23:50 +0000

    Managed physics system cleanup and refactor
    Former-commit-id: a416ecd00f6235cb9973d164ec6cd9d00e1a78c7

--- WorldsEngineManaged/Physics/ICollisionHandler.cs
+++ WorldsEngineManaged/Physics/ICollisionHandler.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace WorldsEngine
 {
+    /// <summary>
+    /// Interface for a component that wants to receive collision events.
+    /// </summary>
     public interface ICollisionHandler
     {
+        /// <summary>
+        /// Callback for a collision.
+        /// </summary>
+        /// <param name="entity">The entity this component is attached to.</param>
+        /// <param name="contactInfo">Info regarding the collision.</param>
         void OnCollision(Entity entity, ref PhysicsContactInfo contactInfo);
     }
 }
---
+++ WorldsEngineManaged/Physics/Physics.NativeBindings.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using WorldsEngine.Math;
+
+namespace WorldsEngine
+{
+    public static partial class Physics
+    {
+        [DllImport(WorldsEngine.NativeModule)]
+        private static extern bool physics_raycast(Vector3 origin, Vector3 direction, float maxDist, uint excludeLayerMask, out RaycastHit hit);
+
+        [DllImport(WorldsEngine.NativeModule)]
+        private static extern bool physics_overlapSphere(Vector3 origin, float radius, out uint overlappedEntity);
+
+        [DllImport(WorldsEngine.NativeModule)]
+        private static unsafe extern uint physics_overlapSphereMultiple(Vector3 origin, float radius, uint maxTouchCount, Entity* entityBuffer, uint excludeLayerMask);
+
+        [DllImport(WorldsEngine.NativeModule)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static unsafe extern bool physics_sweepSphere(Vector3 origin, float radius, Vector3 direction, float distance, out RaycastHit hit, uint excludeLayerMask);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void NativeCollisionDelegate(uint entityId, uint id, ref PhysicsContactInfo contactInfo);
+
+        [DllImport(WorldsEngine.NativeModule)]
+        private static extern void physics_addEventHandler(NativeCollisionDelegate collisionDelegate);
+    }
+}
