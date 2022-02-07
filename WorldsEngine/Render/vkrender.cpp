5612c691376eedbc7c82bcde4e1997fe2712e1fd
Someone Somewhere <someone167@pm.me>
2022-02-07 17:13:17 +0000

    Move deletion queue to separate file
    Former-commit-id: a6c6abadd0f4f7e55ddeed30c59626a38407d51a

--- WorldsEngine/Render/vkrender.cpp
+++ WorldsEngine/Render/vkrender.cpp
@@ -84,101 +84,6 @@ uint32_t findPresentQueue(VkPhysicalDevice pd, VkSurfaceKHR surface) {
     return ~0u;
 }
 
-std::vector<DeletionQueue::DQueue> DeletionQueue::deletionQueues;
-uint32_t DeletionQueue::currentFrameIndex = 0u;
-VkDevice deletionDevice = VK_NULL_HANDLE;
-VmaAllocator deletionAllocator = nullptr;
-
-void DeletionQueue::queueObjectDeletion(void* object, VkObjectType type) {
-    deletionQueues[currentFrameIndex].objectDeletions.emplace_back(object, type);
-
-}
-
-void DeletionQueue::queueMemoryFree(VmaAllocation allocation) {
-    deletionQueues[currentFrameIndex].memoryFrees.emplace_back(allocation);
-}
-
-void DeletionQueue::queueDescriptorSetFree(VkDescriptorPool pool, VkDescriptorSet set) {
-    deletionQueues[currentFrameIndex].dsFrees.emplace_back(pool, set);
-}
-
-void DeletionQueue::setCurrentFrame(uint32_t frame) {
-    currentFrameIndex = frame;
-}
-
-void DeletionQueue::cleanupFrame(uint32_t frame) {
-    DQueue& queue = deletionQueues[currentFrameIndex];
-
-    for (const ObjectDeletion& od : queue.objectDeletions) {
-        processObjectDeletion(od);
-    }
-
-    for (const MemoryFree& mf : queue.memoryFrees) {
-        processMemoryFree(mf);
-    }
-
-    for (const DescriptorSetFree& dsf : queue.dsFrees) {
-        vkFreeDescriptorSets(deletionDevice, dsf.desciptorPool, 1, &dsf.descriptorSet);
-    }
-
-    queue.objectDeletions.clear();
-    queue.memoryFrees.clear();
-    queue.dsFrees.clear();
-}
-
-void DeletionQueue::resize(uint32_t maxFrames) {
-    deletionQueues.resize(maxFrames);
-}
-
-void DeletionQueue::processObjectDeletion(const ObjectDeletion& od) {
-    void* object = od.object;
-    switch (od.type) {
-    case VK_OBJECT_TYPE_EVENT:
-        vkDestroyEvent(deletionDevice, (VkEvent)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_PIPELINE:
-        vkDestroyPipeline(deletionDevice, (VkPipeline)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_SAMPLER:
-        vkDestroySampler(deletionDevice, (VkSampler)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_PIPELINE_LAYOUT:
-        vkDestroyPipelineLayout(deletionDevice, (VkPipelineLayout)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_DESCRIPTOR_SET_LAYOUT:
-        vkDestroyDescriptorSetLayout(deletionDevice, (VkDescriptorSetLayout)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_SHADER_MODULE:
-        vkDestroyShaderModule(deletionDevice, (VkShaderModule)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_RENDER_PASS:
-        vkDestroyRenderPass(deletionDevice, (VkRenderPass)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_FRAMEBUFFER:
-        vkDestroyFramebuffer(deletionDevice, (VkFramebuffer)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_DESCRIPTOR_POOL:
-        vkDestroyDescriptorPool(deletionDevice, (VkDescriptorPool)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_BUFFER:
-        vkDestroyBuffer(deletionDevice, (VkBuffer)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_IMAGE:
-        vkDestroyImage(deletionDevice, (VkImage)object, nullptr);
-        break;
-    case VK_OBJECT_TYPE_IMAGE_VIEW:
-        vkDestroyImageView(deletionDevice, (VkImageView)object, nullptr);
-        break;
-    default:
-        fatalErr("Unhandled Vulkan object deletion! This is a bug.");
-        break;
-    }
-}
-
-void DeletionQueue::processMemoryFree(const MemoryFree& mf) {
-    vmaFreeMemory(deletionAllocator, mf.allocation);
-}
-
 RenderResource* VKRenderer::createTextureResource(TextureResourceCreateInfo resourceCreateInfo, const char* debugName) {
     RenderResource* resource = new RenderResource;
 
@@ -627,10 +532,8 @@ VKRenderer::VKRenderer(const RendererInitInfo& initInfo, bool* success)
     vk11Features.pNext = &vk12Features;
 
     device = dm.create(physicalDevice);
-    deletionDevice = device;
 
     ShaderCache::setDevice(device);
-    DeletionQueue::resize(1);
 
     queues.graphics = getQueue(device, queues.graphicsIdx);
     queues.present = getQueue(device, queues.presentIdx);
@@ -646,7 +549,8 @@ VKRenderer::VKRenderer(const RendererInitInfo& initInfo, bool* success)
     allocatorCreateInfo.flags = 0;
     vmaCreateAllocator(&allocatorCreateInfo, &allocator);
 
-    deletionAllocator = allocator;
+    DeletionQueue::intitialize(device, allocator);
+    DeletionQueue::resize(1);
 
     VkPipelineCacheCreateInfo pipelineCacheInfo{VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO};
     PipelineCacheSerializer::loadPipelineCache(getPhysicalDeviceProperties(physicalDevice), pipelineCacheInfo);
