6cf150c215293f9299e7675fa7a5afe1f2fdf47e
Someone Somewhere <someone167@pm.me>
2022-02-24 18:00:05 +0000

    Texture editor+compiler stuff

--- WorldsEngine/AssetCompilation/TextureCompiler.cpp
+++ WorldsEngine/AssetCompilation/TextureCompiler.cpp
@@ -60,11 +60,41 @@ namespace worlds {
         }
     }
 
+    void TextureAssetSettings::initialiseForType(TextureAssetType t) {
+        type = t;
+        switch (t) {
+        case TextureAssetType::Crunch:
+            crunch.sourceTexture = INVALID_ASSET;
+            crunch.isNormalMap = false;
+            crunch.isSrgb = true;
+            crunch.qualityLevel = 127;
+            break;
+        case TextureAssetType::RGBA:
+            rgba.sourceTexture = INVALID_ASSET;
+            rgba.isSrgb = true;
+            break;
+        case TextureAssetType::PBR:
+            pbr.roughnessSource = INVALID_ASSET;
+            pbr.metallicSource = INVALID_ASSET;
+            pbr.occlusionSource = INVALID_ASSET;
+            pbr.normalMap = INVALID_ASSET;
+            pbr.normalRoughnessMipStrength = 0.0f;
+            pbr.defaultRoughness = 0.5f;
+            pbr.defaultMetallic = 0.0f;
+            pbr.defaultOcclusion = 1.0f;
+            pbr.qualityLevel = 127;
+            break;
+        }
+    }
+
     void loadCrunchSettings(TextureAssetSettings& settings, nlohmann::json& j) {
         settings.crunch.isNormalMap = j.value("isNormalMap", false);
         settings.crunch.qualityLevel = j.value("qualityLevel", 127);
         settings.crunch.isSrgb = j.value("isSrgb", true);
-        settings.crunch.sourceTexture = AssetDB::pathToId(j["sourceTexture"].get<std::string>());
+        if (j.contains("sourceTexture"))
+            settings.crunch.sourceTexture = AssetDB::pathToId(j["sourceTexture"].get<std::string>());
+        else
+            settings.crunch.sourceTexture = INVALID_ASSET;
     }
 
     void loadRGBASettings(TextureAssetSettings& settings, nlohmann::json& j) {
@@ -76,17 +106,35 @@ namespace worlds {
         settings.pbr.defaultMetallic = j.value("defaultMetallic", 0.0f);
         settings.pbr.defaultRoughness = j.value("defaultRoughness", 0.0f);
         settings.pbr.defaultOcclusion = j.value("defaultOcclusion", 0.0f);
-        settings.pbr.metallicSource = AssetDB::pathToId(j["metallicSource"].get<std::string>());
-        settings.pbr.roughnessSource = AssetDB::pathToId(j["roughnessSource"].get<std::string>());
-        settings.pbr.occlusionSource = AssetDB::pathToId(j["occlusionSource"].get<std::string>());
-        settings.pbr.normalMap = AssetDB::pathToId(j["normalMap"].get<std::string>());
+
+        if (j.contains("metallicSource"))
+            settings.pbr.metallicSource = AssetDB::pathToId(j["metallicSource"].get<std::string>());
+        else
+            settings.pbr.metallicSource = INVALID_ASSET;
+
+        if (j.contains("roughnessSource"))
+            settings.pbr.roughnessSource = AssetDB::pathToId(j["roughnessSource"].get<std::string>());
+        else
+            settings.pbr.roughnessSource = INVALID_ASSET;
+
+        if (j.contains("occlusionSource"))
+            settings.pbr.occlusionSource = AssetDB::pathToId(j["occlusionSource"].get<std::string>());
+        else
+            settings.pbr.occlusionSource = INVALID_ASSET;
+
+        if (j.contains("normalMap"))
+            settings.pbr.normalMap = AssetDB::pathToId(j["normalMap"].get<std::string>());
+        else
+            settings.pbr.normalMap = INVALID_ASSET;
+
         settings.pbr.normalRoughnessMipStrength = j.value("normalRoughnessMipStrength", 0.0f);
+        settings.pbr.qualityLevel = j.value("qualityLevel", 127);
     }
 
     TextureAssetSettings TextureAssetSettings::fromJson(nlohmann::json& j) {
         TextureAssetSettings s{};
 
-        s.type = getAssetType(j["type"]);
+        s.type = getAssetType(j.value("type", "crunch"));
 
         switch (s.type) {
         case TextureAssetType::Crunch:
@@ -111,6 +159,28 @@ namespace worlds {
         j["sourceTexture"] = AssetDB::idToPath(cts.sourceTexture);
     }
 
+    void savePBRSettings(TextureAssetSettings& settings, nlohmann::json& j) {
+        PBRTextureSettings& pbs = settings.pbr;
+        j["defaultMetallic"] = pbs.defaultMetallic;
+        j["defaultRoughness"] = pbs.defaultRoughness;
+        j["defaultOcclusion"] = pbs.defaultOcclusion;
+
+        if (pbs.metallicSource != INVALID_ASSET) {
+            j["metallicSource"] = AssetDB::idToPath(pbs.metallicSource);
+        }
+
+        if (pbs.roughnessSource != INVALID_ASSET) {
+            j["roughnessSource"] = AssetDB::idToPath(pbs.roughnessSource);
+        }
+
+        if (pbs.occlusionSource != INVALID_ASSET) {
+            j["occlusionSource"] = AssetDB::idToPath(pbs.occlusionSource);
+        }
+
+        j["normalRoughnessMipStrength"] = pbs.normalRoughnessMipStrength;
+        j["qualityLevel"] = pbs.qualityLevel;
+    }
+
     void TextureAssetSettings::toJson(nlohmann::json& j) {
         j["type"] = getAssetTypeString(type);
 
@@ -121,6 +191,7 @@ namespace worlds {
         case TextureAssetType::RGBA:
             break;
         case TextureAssetType::PBR:
+            savePBRSettings(*this, j);
             break;
         }
     }
@@ -167,7 +238,24 @@ namespace worlds {
         }
 
         nlohmann::json j = nlohmann::json::parse(jsonContents.value);
-        out.push_back(j["sourceTexture"]);
+
+        TextureAssetSettings tas = TextureAssetSettings::fromJson(j);
+
+        switch (tas.type) {
+        case TextureAssetType::Crunch:
+            out.push_back(j["sourceTexture"]);
+            break;
+        case TextureAssetType::PBR:
+            if (j.contains("metallicSource"))
+                out.push_back(j["metallicSource"]);
+
+            if (j.contains("roughnessSource"))
+                out.push_back(j["roughnessSource"]);
+
+            if (j.contains("occlusionSource"))
+                out.push_back(j["occlusionSource"]);
+            break;
+        }
     }
 
 
@@ -187,7 +275,7 @@ namespace worlds {
 
     void TextureCompiler::compileCrunch(TexCompileThreadInfo* tcti) {
         const CrunchTextureSettings& cts = tcti->assetSettings.crunch;
-        AssetID sourceId = cts.sourceTexture; 
+        AssetID sourceId = cts.sourceTexture;
         bool isSrgb = cts.isSrgb;
         RawTextureData inTexData;
 
@@ -256,6 +344,116 @@ namespace worlds {
         }
     }
 
+    void TextureCompiler::compilePBR(TexCompileThreadInfo* tcti) {
+        assert(tcti->assetSettings.type == TextureAssetType::PBR);
+        const PBRTextureSettings& pts = tcti->assetSettings.pbr;
+
+        RawTextureData roughnessData;
+        RawTextureData metallicData;
+        RawTextureData occlusionData;
+        bool useRoughness = false;
+        bool useMetallic = false;
+        bool useOcclusion = false;
+
+        if (pts.roughnessSource != INVALID_ASSET) {
+            if (RawTextureLoader::loadRawTexture(pts.roughnessSource, roughnessData)) {
+                useRoughness = true;
+            } else {
+                logErr("Failed to load roughness layer");
+            }
+        }
+
+        if (pts.metallicSource != INVALID_ASSET) {
+            if (RawTextureLoader::loadRawTexture(pts.metallicSource, metallicData)) {
+                useMetallic = true;
+            } else {
+                logErr("Failed to load metallic layer");
+            }
+        }
+
+        if (pts.occlusionSource != INVALID_ASSET) {
+            if (RawTextureLoader::loadRawTexture(pts.occlusionSource, occlusionData)) {
+                useMetallic = true;
+            } else {
+                logErr("Failed to load occlusion layer");
+            }
+        }
+
+        int outWidth = roughnessData.width;
+        int outHeight = roughnessData.height;
+
+        if (useMetallic) {
+            if (metallicData.width != outWidth || metallicData.height != outHeight)
+                logErr("Metallic texture size doesn't match!");
+        }
+
+        uint8_t* layeredData = new uint8_t[outWidth * outHeight * 4];
+
+        for (int x = 0; x < outWidth; x++) {
+            for (int y = 0; y < outHeight; y++) {
+                int baseIdx = 4 * (x + y * outWidth);
+
+                // Red channel - Metallic
+                if (useMetallic)
+                    layeredData[baseIdx + 0] = static_cast<uint8_t*>(metallicData.data)[baseIdx + 0];
+                else
+                    layeredData[baseIdx + 0] = 255 * pts.defaultMetallic;
+
+                // Green channel - Roughness
+                if (useRoughness)
+                    layeredData[baseIdx + 1] = static_cast<uint8_t*>(roughnessData.data)[baseIdx + 1];
+                else
+                    layeredData[baseIdx + 1] = 255 * pts.defaultRoughness;
+
+                // Red channel - Occlusion
+                if (useOcclusion)
+                    layeredData[baseIdx + 2] = static_cast<uint8_t*>(occlusionData.data)[baseIdx + 2];
+                else
+                    layeredData[baseIdx + 2] = 255;//255 * pts.defaultOcclusion;
+
+                // Alpha channel is currently unused, set it to 255
+                layeredData[baseIdx + 3] = 255;
+            }
+        }
+
+        crn_comp_params compParams;
+        compParams.m_width = outWidth;
+        compParams.m_height = outHeight;
+        compParams.set_flag(cCRNCompFlagPerceptual, false);
+        compParams.m_file_type = cCRNFileTypeCRN;
+        compParams.m_format = cCRNFmtDXT5;
+        compParams.m_pImages[0][0] = (crn_uint32*)layeredData;
+        compParams.m_quality_level = pts.qualityLevel;
+        compParams.m_num_helper_threads = SDL_GetCPUCount() - 1;
+        compParams.m_pProgress_func = progressCallback;
+        compParams.m_pProgress_func_data = tcti->compileOp;
+        compParams.m_userdata0 = false;
+        logMsg("perceptual: %i, format: %i", compParams.get_flag(cCRNCompFlagPerceptual), compParams.m_format);
+
+        crn_mipmap_params mipParams;
+        mipParams.m_gamma_filtering = false;
+        mipParams.m_mode = cCRNMipModeGenerateMips;
+
+        crn_uint32 outputSize;
+        float actualBitrate;
+        crn_uint32 actualQualityLevel;
+
+        void* outData = crn_compress(compParams, mipParams, outputSize, &actualQualityLevel, &actualBitrate);
+        logMsg("Compressed %s with actual quality of %u and bitrate of %f", tcti->inputPath.c_str(), actualQualityLevel, actualBitrate);
+
+        int nMips = std::floor(std::log2(std::max(outWidth, outHeight))) + 1;
+
+        writeCrunchedWtex(tcti, false, outWidth, outHeight, nMips, outData, outputSize);
+
+        crn_free_block(outData);
+
+        delete[] layeredData;
+
+        tcti->compileOp->progress = 1.0f;
+        tcti->compileOp->complete = true;
+        tcti->compileOp->result = CompilationResult::Success;
+    }
+
     void TextureCompiler::writeCrunchedWtex(TexCompileThreadInfo* tcti, bool isSrgb, int width, int height, int nMips, void* data, size_t dataSize) {
         wtex::Header header{};
         header.containedFormat = wtex::ContainedFormat::Crunch;
@@ -290,9 +488,7 @@ namespace worlds {
             tcti->compileOp->result = CompilationResult::Error;
             break;
         case TextureAssetType::PBR:
-            logErr("Currently unsupported :(");
-            tcti->compileOp->complete = true;
-            tcti->compileOp->result = CompilationResult::Error;
+            compilePBR(tcti);
             break;
         }
     }
