fd25cb11f9ada040d8b9fa0f485e86c0a078cecf
Someone Somewhere <someone167@pm.me>
2022-03-07 17:22:54 +0000

    Turret logic

--- Game/Turret.cs
+++ Game/Turret.cs
@@ -9,6 +9,7 @@ namespace Game;
 public class Turret : Component, IStartListener, IThinkingComponent
 {
     private Transform _initialPitchTransform;
+    private Transform _initialYawTransform;
     public void Start()
     {
         var swo = Entity.GetComponent<SkinnedWorldObject>();
@@ -19,17 +20,32 @@ public class Turret : Component, IStartListener, IThinkingComponent
         }
 
         _initialPitchTransform = MeshManager.GetBoneRestTransform(swo.Mesh, MeshManager.GetBoneIndex(swo.Mesh, "PitchPivot"));
+        _initialYawTransform = MeshManager.GetBoneRestTransform(swo.Mesh, MeshManager.GetBoneIndex(swo.Mesh, "YawPivot"));
     }
 
     public void Think()
     {
+        var transform = Entity.Transform;
         var swo = Entity.GetComponent<SkinnedWorldObject>();
         uint pitchBoneIdx = MeshManager.GetBoneIndex(swo.Mesh, "PitchPivot");
+        uint yawBoneIdx = MeshManager.GetBoneIndex(swo.Mesh, "YawPivot");
+
+        Vector3 gunPos = transform.Position + new Vector3(0.0f, 0.875f, 0.0f);
+        Vector3 target = Camera.Main.Position;
+        float distance = gunPos.DistanceTo(target);
+        Vector3 direction = (target - gunPos).Normalized;
+
+        float pitchAngle = MathF.Asin(direction.y);
+        float yawAngle = MathF.Atan2(direction.x, direction.z);
 
         Transform pitchPivotTransform = _initialPitchTransform; 
-        float rotationT = (float)Math.Sin(Time.CurrentTime) * 0.5f + 0.5f;
-        Quaternion rotation = Quaternion.AngleAxis(rotationT * MathF.PI - (MathF.PI / 2.0f), Vector3.Forward);
+        Quaternion rotation = Quaternion.AngleAxis(pitchAngle - (MathF.PI * 0.5f), Vector3.Forward);
         pitchPivotTransform.Rotation = rotation;
         swo.SetBoneTransform(pitchBoneIdx, pitchPivotTransform);
+
+        Transform yawPivotTransform = _initialYawTransform; 
+        Quaternion yawRotation = Quaternion.AngleAxis(yawAngle - (MathF.PI * 0.5f), Vector3.Up);
+        yawPivotTransform.Rotation = yawRotation;
+        swo.SetBoneTransform(yawBoneIdx, yawPivotTransform);
     }
 }
