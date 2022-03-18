67556bc315dfee4c791a2071af7e6aa09179828f
Someone Somewhere <someone167@pm.me>
2022-03-18 00:50:10 +0000

    Patch tracer changes

--- WorldsEngine/PathTracer/PathTracer.hpp
+++ WorldsEngine/PathTracer/PathTracer.hpp
@@ -10,6 +10,7 @@ namespace worlds {
         glm::vec3 direct;
         glm::vec3 indirect;
     };
+    struct RenderTile;
 
     class PathTracer {
     private:
@@ -17,7 +18,8 @@ namespace worlds {
         RTCDevice device;
         RTCScene scene;
         bool traceRay(glm::vec3 pos, glm::vec3 dir, RTCRayHit& rayHit);
-        PathTraceResult pathTrace(glm::vec3 pos, glm::vec3 dir);
+        PathTraceResult pathTrace(glm::vec3 pos, glm::vec3 dir, int depth = 0);
+        void traceTile(RenderTile& rt);
     public:
         PathTracer(entt::registry& reg);
         ~PathTracer();
