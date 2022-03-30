e683d8a8ce0f7dcf5685d3cdfe6bb1119c67c445
Someone Somewhere <someone167@pm.me>
2022-03-30 17:47:33 +0100

    Slight code cleanup
    Former-commit-id: ad1b60ab9f3eba94039f507d1cc1a98be4b32bd5

---
+++ WorldsEngine/Editor/Widgets/IntegratedMenubar.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <Core/IGameEventHandler.hpp>
+
+namespace worlds {
+    class IntegratedMenubar {
+    public:
+        IntegratedMenubar(EngineInterfaces interfaces);
+        void draw();
+    private:
+        EngineInterfaces interfaces;
+    };
+}
