8070eee6b7cf820475d0a04bc2e504e2f617a0cb
Someone Somewhere <someone167@pm.me>
2022-04-21 00:43:40 +0100

    more skeleton stuff
    Former-commit-id: c7c7fc7125fc0c2692c9bea8d91ef672fd18c7bb

--- Game/Player/PlayerSkeletonMatch.cs
+++ Game/Player/PlayerSkeletonMatch.cs
@@ -1,3 +1,4 @@
+using System;
 using WorldsEngine;
 using WorldsEngine.Math;
 
@@ -8,6 +9,7 @@ public class PlayerSkeletonMatch : Component, IStartListener, IUpdateableCompone
 {
     private Transform _initialT;
     private Transform _lhToWorld = new(Vector3.Zero, Quaternion.Identity);
+    private Transform _rhToWorld = new(Vector3.Zero, Quaternion.Identity);
 
     public void Start()
     {
@@ -17,21 +19,45 @@ public class PlayerSkeletonMatch : Component, IStartListener, IUpdateableCompone
 
         var mesh = MeshManager.GetMesh(swo.Mesh);
         var lh = MeshManager.GetBoneIndex(swo.Mesh, "hand_L");
+        var rh = MeshManager.GetBoneIndex(swo.Mesh, "hand_R");
 
         int parentIdx = mesh.BoneParentIndex((int)lh);
 
         while (parentIdx != -1)
         {
             _lhToWorld = _lhToWorld.TransformBy(swo.GetBoneTransform((uint)parentIdx));
+            parentIdx = mesh.BoneParentIndex(parentIdx);
+        }
+
+        parentIdx = mesh.BoneParentIndex((int)rh);
+
+        while (parentIdx != -1)
+        {
+            _rhToWorld = _rhToWorld.TransformBy(swo.GetBoneTransform((uint)parentIdx));
+            parentIdx = mesh.BoneParentIndex(parentIdx);
         }
     }
     public void Update()
     {
         var swo = Entity.GetComponent<SkinnedWorldObject>();
         var root = MeshManager.GetBoneIndex(swo.Mesh, "root");
-        var rt = MeshManager.GetBoneRestPose(swo.Mesh, root);
-        swo.SetBoneTransform(MeshManager.GetBoneIndex(swo.Mesh, "root"), new Transform(Vector3.Down * 0.9f, _initialT.Rotation));
+        var rootTransform = new Transform(Vector3.Down * 0.9f, _initialT.Rotation);
+        swo.SetBoneTransform(MeshManager.GetBoneIndex(swo.Mesh, "root"), rootTransform);
         var lh = MeshManager.GetBoneIndex(swo.Mesh, "hand_L");
-        swo.SetBoneTransform(lh, LocalPlayerSystem.LeftHand.Transform.TransformByInverse(Entity.Transform).TransformByInverse(_lhToWorld));
+        var rh = MeshManager.GetBoneIndex(swo.Mesh, "hand_R");
+        
+        {
+            var wsTarget = LocalPlayerSystem.LeftHand.Transform;
+            wsTarget.Position += Vector3.Up * 0.9f;
+            wsTarget.Rotation *= new Quaternion(new Vector3(MathF.PI * 0.25f, 0f, MathF.PI * 0.25f));
+            swo.SetBoneTransform(lh, wsTarget.TransformByInverse(Entity.Transform).TransformByInverse(_lhToWorld));
+        }
+
+        {
+            var wsTarget = LocalPlayerSystem.RightHand.Transform;
+            wsTarget.Position += Vector3.Up * 0.9f;
+            wsTarget.Rotation *= new Quaternion(new Vector3(MathF.PI * 0.25f, 0f, MathF.PI * 0.25f));
+            swo.SetBoneTransform(rh, wsTarget.TransformByInverse(Entity.Transform).TransformByInverse(_rhToWorld));
+        }
     }
 }
