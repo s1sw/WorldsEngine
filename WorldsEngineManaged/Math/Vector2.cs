318a736d31436e33b9422e9ccdf358a3019bc468
Someone Somewhere <someone167@pm.me>
2022-04-29 18:41:13 +0100

    Vector2 max component
    Former-commit-id: db85018c6d2ee557c438b09ddd82658cf100e1ce

--- WorldsEngineManaged/Math/Vector2.cs
+++ WorldsEngineManaged/Math/Vector2.cs
@@ -19,6 +19,8 @@ namespace WorldsEngine.Math
             }
         }
 
+        public float MaxComponent => MathF.Max(x, y);
+
         public float x, y;
 
         public Vector2(float x, float y)
