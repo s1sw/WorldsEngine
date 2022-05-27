4fa096f7a82f36746ff5816e809e84083050b998
Someone Somewhere <someone167@pm.me>
2022-05-27 20:11:50 +0100

    Circular motion script touchup

--- Game/World/CircularMotion.cs
+++ Game/World/CircularMotion.cs
@@ -7,22 +7,19 @@ namespace Game.World;
 [Component]
 class CircularMotion : Component, IThinkingComponent, IStartListener
 {
-    static Random rng = new();
+    public float Radius = 5.0f;
     private Transform _initialTransform;
     private double _offset = 0.0f;
 
     public void Start()
     {
         _initialTransform = Registry.GetTransform(Entity);
-        _offset = rng.NextDouble() * 5.0;
+        _offset = Random.Shared.NextDouble() * 5.0;
     }
 
     public void Think()
     {
-        const float Radius = 5.0f;
 
-        WorldLight light = Registry.GetComponent<WorldLight>(Entity);
-        //light.Intensity = (float)Math.Sin(Time.CurrentTime + _offset) + 3.5f;
         Transform nextT = _initialTransform;
         nextT.Position = _initialTransform.Position + new Vector3(
             (float)Math.Sin((Time.CurrentTime + _offset) * 2.0) * Radius,
