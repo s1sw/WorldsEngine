a0eb87f4696d1ff0747a76ff282ff60c9bdaa107
Someone Somewhere <someone167@pm.me>
2022-05-30 11:06:43 +0100

    Tiny formatting change
    Former-commit-id: 92d547e3f57cf09e25f9928d6ade7cbe9820e1a3

--- WorldsEngine/ComponentMeta/ComponentEditorUtil.hpp
+++ WorldsEngine/ComponentMeta/ComponentEditorUtil.hpp
@@ -7,12 +7,12 @@ namespace worlds {
     template <typename T>
     class BasicComponentUtil : public ComponentEditor {
     private:
-        template < typename = typename std::is_default_constructible<T>::type>
+        template <typename = typename std::is_default_constructible<T>::type>
         void createInternal(entt::entity ent, entt::registry& reg) {
             reg.emplace<T>(ent);
         }
 
-        template < typename = typename std::is_copy_constructible<T>::type>
+        template <typename = typename std::is_copy_constructible<T>::type>
         void cloneInternal(entt::entity from, entt::entity to, entt::registry& reg) {
             reg.emplace<T>(to, reg.get<T>(from));
         }
