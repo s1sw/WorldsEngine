e3e78282cc1230fe6fd6784496a73c8081edd038
Someone Somewhere <someone167@pm.me>
2022-08-05 13:27:38 +0100

    Misc fixes

--- R2/Source/VK/VMAImplementation.cpp
+++ R2/Source/VK/VMAImplementation.cpp
@@ -2,10 +2,10 @@
 #include <volk.h>
 R2::VK::IDebugOutputReceiver* vmaDebugOutputRecv;
 
-#define VMA_DEBUG_LOG(format, ...) do { \
-    char buf[512]; \
-    snprintf(buf, 512, format, __VA_ARGS__); \
-    vmaDebugOutputRecv->DebugMessage(buf); \
-} while(false)
+//#define VMA_DEBUG_LOG(format, ...) do { \
+//    char buf[512]; \
+//    snprintf(buf, 512, format, __VA_ARGS__); \
+//    vmaDebugOutputRecv->DebugMessage(buf); \
+//} while(false)
 #define VMA_IMPLEMENTATION
 #include <vk_mem_alloc.h>
