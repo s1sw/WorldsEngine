6b7bba914baf0eeb4d1f31240a162508230ff8d4
Someone Somewhere <someone167@pm.me>
2022-08-23 20:54:14 +0100

    Add more buttons to Game Controls
    Former-commit-id: c2fa0b7fec945013961c2341bf157ffa9fad21db

--- WorldsEngine/Editor/EditorWindows/GameControls.cpp
+++ WorldsEngine/Editor/EditorWindows/GameControls.cpp
@@ -9,9 +9,41 @@ namespace worlds
     {
         if (ImGui::Begin(ICON_FA_GAMEPAD u8" Game Controls", &active))
         {
-            if (ImGui::Button((const char*)(ICON_FA_PLAY_CIRCLE u8" Play")))
+
+            switch (editor->getCurrentState())
             {
-                g_console->executeCommandStr("play");
+            case GameState::Editing:
+                if (ImGui::Button(ICON_FA_PLAY_CIRCLE u8" Play"))
+                {
+                    g_console->executeCommandStr("play");
+                }
+                break;
+            case GameState::Playing:
+                if (ImGui::Button(ICON_FA_STOP_CIRCLE u8" Stop"))
+                {
+                    g_console->executeCommandStr("reloadAndEdit");
+                }
+
+                ImGui::SameLine();
+
+                if (ImGui::Button(ICON_FA_PAUSE_CIRCLE u8" Pause"))
+                {
+                    g_console->executeCommandStr("pauseAndEdit");
+                }
+                break;
+            case GameState::Paused:
+                if (ImGui::Button(ICON_FA_STOP_CIRCLE u8" Stop"))
+                {
+                    g_console->executeCommandStr("reloadAndEdit");
+                }
+
+                ImGui::SameLine();
+
+                if (ImGui::Button(ICON_FA_PAUSE_CIRCLE u8" Unpause"))
+                {
+                    g_console->executeCommandStr("unpause");
+                }
+                break;
             }
         }
         ImGui::End();
