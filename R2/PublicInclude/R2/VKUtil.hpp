3936da3a9a9a365a7730a672ba48e647672b2742
Someone Somewhere <someone167@pm.me>
2022-09-04 11:00:11 +0100

    Add mipScale utility
    Former-commit-id: 083fe328877bd231c2852cec59369989cb3cf7db

--- R2/PublicInclude/R2/VKUtil.hpp
+++ R2/PublicInclude/R2/VKUtil.hpp
@@ -23,4 +23,16 @@ namespace R2::VK
     enum class AccessFlags : uint64_t;
     enum class PipelineStageFlags : uint64_t;
     PipelineStageFlags getPipelineStage(AccessFlags access);
+
+    inline int mipScale(int val, int mip)
+    {
+        int scaled = val >> mip;
+        return scaled > 1 ? scaled : 1;
+    }
+
+    inline uint32_t mipScale(uint32_t val, uint32_t mip)
+    {
+        uint32_t scaled = val >> mip;
+        return scaled > 1 ? scaled : 1;
+    }
 }
