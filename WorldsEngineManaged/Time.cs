761c3a63549d36531f1db0ccb70e239169186f36
Someone Somewhere <someone167@pm.me>
2022-09-17 15:11:22 +0100

    Fix awaitables infinite loop
    Former-commit-id: 37d437f9cdad4757bfbd4c656a0bb5419abcd4b0

--- WorldsEngineManaged/Time.cs
+++ WorldsEngineManaged/Time.cs
@@ -15,28 +15,35 @@ namespace WorldsEngine
         public static float InterpolationAlpha { get; internal set; }
     }
 
-    public class EngineEventAwaiter : INotifyCompletion
+    public class EngineEventAwaitable : INotifyCompletion
     {
-        internal Queue<Action> _queuedContinuations = new();
+        // Use two queues and swap so we don't end up in an infinite loop
+        internal Queue<Action> _queuedContinuationsA = new();
+        internal Queue<Action> _queuedContinuationsB = new();
+        internal bool _useQueueB = false;
+
+        internal Queue<Action> currentQueue => _useQueueB ? _queuedContinuationsB : _queuedContinuationsA;
 
         public bool IsCompleted => false;
 
         public void OnCompleted(Action continuation)
         {
-            _queuedContinuations.Enqueue(continuation);
+            currentQueue.Enqueue(continuation);
         }
 
         public void GetResult() { }
 
         internal void Run()
         {
-            while (_queuedContinuations.Count > 0)
+            Queue<Action> cq = currentQueue;
+            _useQueueB = !_useQueueB;
+            while (cq.Count > 0)
             {
-                Action a = _queuedContinuations.Dequeue();
+                Action a = cq.Dequeue();
                 a.Invoke();
             }
 
-            _queuedContinuations.Clear();
+            cq.Clear();
         }
     }
 
@@ -55,7 +62,7 @@ namespace WorldsEngine
 
     public static class Awaitables
     {
-        public readonly static AwaitableWrapper<EngineEventAwaiter> NextFrame = new(new EngineEventAwaiter());
-        public readonly static AwaitableWrapper<EngineEventAwaiter> NextSimulationTick = new(new EngineEventAwaiter());
+        public readonly static AwaitableWrapper<EngineEventAwaitable> NextFrame = new(new EngineEventAwaitable());
+        public readonly static AwaitableWrapper<EngineEventAwaitable> NextSimulationTick = new(new EngineEventAwaitable());
     }
 }
